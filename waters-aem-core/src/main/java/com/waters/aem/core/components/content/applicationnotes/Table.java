1055f1ebf4554d9fabf7eb764730ffea50bc588b
Mark Daugherty <mark.r.daugherty@gmail.com>
2018-12-04 13:48:43 -0600

    added CSV parser stub code.

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/applicationnotes/Table.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/applicationnotes/Table.java
@@ -1,13 +1,69 @@
 package com.waters.aem.core.components.content.applicationnotes;
 
 import com.citytechinc.cq.component.annotations.Component;
+import com.citytechinc.cq.component.annotations.DialogField;
+import com.citytechinc.cq.component.annotations.widgets.TextArea;
+import com.google.common.collect.HashBasedTable;
 import com.waters.aem.core.constants.WatersConstants;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
 
 @Component(value = "Table", path = WatersConstants.COMPONENT_PATH_APPLICATION_NOTES)
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public final class Table {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Table.class);
+
+    @DialogField(fieldLabel = "CSV Data")
+    @TextArea
+    @Inject
+    private String csvData;
+
+    public Collection<Map<String, String>> getTableRows() {
+        return getTable().rowMap().values();
+    }
+
+    public com.google.common.collect.Table<Integer, String, String> getTable() {
+        final com.google.common.collect.Table<Integer, String, String> table = HashBasedTable.create();
+
+        if (csvData != null) {
+            try {
+                final CSVParser parser = CSVParser.parse(csvData, CSVFormat.EXCEL
+                    .withFirstRecordAsHeader()
+                    .withTrim());
+
+                // TODO determine how to handle header row
+
+                final Map<String, Integer> headerMap = parser.getHeaderMap();
+
+                int columnKey = 0;
+
+                for (final CSVRecord record : parser.getRecords()) {
+                    for (final String columnName : headerMap.keySet()) {
+                        final Integer columnIndex = headerMap.get(columnName);
+                        final String value = record.get(columnIndex);
+
+                        table.put(columnKey, columnName, value);
+                    }
+
+                    columnKey++;
+                }
+            } catch (IOException e) {
+                LOG.error("error parsing CSV data", e);
+            }
+        }
+
+        return table;
+    }
 }
---
+++ waters-aem-core/src/test/groovy/com/waters/aem/core/components/content/applicationnotes/TableSpec.groovy
@@ -0,0 +1,47 @@
+package com.waters.aem.core.components.content.applicationnotes
+
+import com.icfolson.aem.library.models.specs.AemLibraryModelSpec
+
+class TableSpec extends AemLibraryModelSpec {
+
+    private static final String CSV_DATA = """first_name,last_name,email
+Cristobal,Lindberg,clindberg0@youku.com
+Tommi,Nanuccioi,tnanuccioi1@economist.com
+Celene,Mackinder,cmackinder2@economist.com
+Lorens,Falconer,lfalconer3@upenn.edu
+Alistair,Taggert,ataggert4@behance.net
+Ben,Hanlin,bhanlin5@nymag.com
+Nissy,Chastney,nchastney6@fda.gov
+Zelma,Tiffany,ztiffany7@ftc.gov
+Dorie,Fanton,dfanton8@g.co
+Crichton,Bruffell,cbruffell9@vimeo.com"""
+
+    def setupSpec() {
+        pageBuilder.content {
+            waters {
+                "jcr:content" {
+                    table(csvData: CSV_DATA)
+                }
+            }
+        }
+    }
+
+    def "get table rows"() {
+        setup:
+        def table = getResource("/content/waters/jcr:content/table").adaptTo(Table)
+
+        expect:
+        table.tableRows.size() == 10
+    }
+
+    def "get first table row"() {
+        setup:
+        def table = getResource("/content/waters/jcr:content/table").adaptTo(Table)
+        def firstRow = table.tableRows[0]
+
+        expect:
+        firstRow.get("first_name") == "Cristobal"
+        firstRow.get("last_name") == "Lindberg"
+        firstRow.get("email") == "clindberg0@youku.com"
+    }
+}
