9c7c47ee80f2052b9c3fd9325f269c8440fdefb9
CORP\uslval <Lakshmi_valluripalli@waters.com>
2018-12-04 14:12:02 -0500

    [Incorporated Code review comments]

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/applicationnotes/ExternalArticles.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/applicationnotes/ExternalArticles.java
@@ -3,16 +3,14 @@ package com.waters.aem.core.components.content.applicationnotes;
 import com.citytechinc.cq.component.annotations.Component;
 import com.citytechinc.cq.component.annotations.DialogField;
 import com.icfolson.aem.library.core.components.AbstractComponent;
+import com.icfolson.aem.library.models.annotations.InheritInject;
 import com.icfolson.aem.multicompositeaddon.widget.MultiCompositeField;
 import com.waters.aem.core.constants.WatersConstants;
-import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 
-import javax.inject.Inject;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component(value = "External Articles" , path = WatersConstants.COMPONENT_PATH_APPLICATION_NOTES)
 @Model(adaptables = { Resource.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
@@ -21,13 +19,11 @@ public class ExternalArticles extends AbstractComponent {
     @DialogField(fieldLabel = "Link Items",
             fieldDescription = "Enter external article details")
     @MultiCompositeField
-    @Inject
+    @InheritInject
     List<LinkItem> linkItems;
 
     public List<LinkItem> getLinkItems() {
-        return   getComponentNodes("linkItems").stream()
-                .map( node -> node.getResource().adaptTo(LinkItem.class))
-                .collect(Collectors.toList());
+       return linkItems;
     }
 
 }
--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/applicationnotes/LinkItem.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/applicationnotes/LinkItem.java
@@ -5,6 +5,7 @@ import com.citytechinc.cq.component.annotations.widgets.PathField;
 import com.citytechinc.cq.component.annotations.widgets.TextField;
 import com.icfolson.aem.library.api.link.Link;
 import com.icfolson.aem.library.models.annotations.LinkInject;
+import com.waters.aem.core.constants.WatersConstants;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
@@ -15,13 +16,6 @@ import javax.inject.Inject;
 @Model(adaptables = { Resource.class, SlingHttpServletRequest.class },
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class LinkItem {
-
-    public LinkItem(){};
-
-    public LinkItem(String text, String path) {
-        this.text = text;
-        this.path = path;
-    }
 
     @DialogField(fieldLabel = "Link Item Text",
             fieldDescription = "Enter Link Item Text",
@@ -35,14 +29,13 @@ public class LinkItem {
             fieldDescription = "Enter or Select LinkItem Path",
             required = true,
             ranking = 2)
-    @TextField
-    @Inject
-    private String path;
+    @PathField(rootPath = WatersConstants.ROOT_PATH)
+    @LinkInject
+    private Link path;
 
-    public String getPath() {
+    public Link getPath() {
         return path;
     }
-
 
     public String getText() {
         return text;
