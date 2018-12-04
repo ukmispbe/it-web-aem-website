31edac027aafcf60e81795cf6b634fe57f04224b
Mark Daugherty <mark.r.daugherty@gmail.com>
2018-12-04 11:52:30 -0600

    anchor component placeholder.

---
+++ waters-aem-core/src/test/groovy/com/waters/aem/core/components/content/AnchorSpec.groovy
@@ -0,0 +1,40 @@
+package com.waters.aem.core.components.content
+
+import com.icfolson.aem.library.models.specs.AemLibraryModelSpec
+import com.waters.aem.core.components.content.applicationnotes.SectionContainer
+
+class AnchorSpec extends AemLibraryModelSpec {
+
+    def setupSpec() {
+        pageBuilder.content {
+            waters {
+                "application-notes"() {
+                    test() {
+                        "jcr:content" {
+                            anchor()
+                            section1("sling:resourceType": SectionContainer.RESOURCE_TYPE, title: "Section 1")
+                            text("sling:resourceType": Text.RESOURCE_TYPE)
+                            section2("sling:resourceType": SectionContainer.RESOURCE_TYPE, title: "Section 2")
+                            image("sling:resourceType": Image.RESOURCE_TYPE)
+                            section3("sling:resourceType": SectionContainer.RESOURCE_TYPE, title: "Section 3")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    def "get anchor links"() {
+        setup:
+        def anchor = getResource("/content/waters/application-notes/test/jcr:content/anchor").adaptTo(Anchor)
+
+        expect:
+        anchor.links.size() == 3
+
+        and:
+        anchor.links*.href == ["#section1", "#section2", "#section3"]
+
+        and:
+        anchor.links*.title == ["Section 1", "Section 2", "Section 3"]
+    }
+}
