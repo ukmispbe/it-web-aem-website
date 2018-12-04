021f58abe3ca42306b2d3d79caa7f962c4ae29a6
Mark Daugherty <mark.r.daugherty@gmail.com>
2018-12-04 11:52:43 -0600

    anchor component placeholder.

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/applicationnotes/SectionContainer.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/applicationnotes/SectionContainer.java
@@ -3,6 +3,9 @@ package com.waters.aem.core.components.content.applicationnotes;
 import com.citytechinc.cq.component.annotations.Component;
 import com.citytechinc.cq.component.annotations.DialogField;
 import com.citytechinc.cq.component.annotations.widgets.TextField;
+import com.icfolson.aem.library.api.link.Link;
+import com.icfolson.aem.library.core.components.AbstractComponent;
+import com.icfolson.aem.library.core.link.builders.factory.LinkBuilderFactory;
 import com.waters.aem.core.constants.WatersConstants;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
@@ -12,7 +15,9 @@ import javax.inject.Inject;
 
 @Component(value = "Section Container", path = WatersConstants.COMPONENT_PATH_APPLICATION_NOTES)
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public final class SectionContainer {
+public final class SectionContainer extends AbstractComponent {
+
+    public static final String RESOURCE_TYPE = "waters/components/content/applicationnotes/sectioncontainer";
 
     @DialogField(fieldLabel = "Title",
         required = true,
@@ -23,5 +28,12 @@ public final class SectionContainer {
 
     public String getTitle() {
         return title;
+    }
+
+    public Link getAnchorLink() {
+        // use generated ID for anchor
+        return LinkBuilderFactory.forPath("#" + getId()) // TODO confirm link strategy
+            .setTitle(getTitle())
+            .build();
     }
 }
