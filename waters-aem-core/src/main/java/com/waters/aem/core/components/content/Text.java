add159a1b67988771d9fdd84e4900eaaa0923a9e
Mark Daugherty <mark.r.daugherty@gmail.com>
2018-12-06 09:27:59 -0600

    added jsr305 dependency.

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/Text.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/Text.java
@@ -1,8 +1,10 @@
 package com.waters.aem.core.components.content;
 
+import com.adobe.cq.export.json.ExporterConstants;
 import com.citytechinc.cq.component.annotations.Component;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Via;
 import org.apache.sling.models.annotations.injectorspecific.Self;
@@ -19,6 +21,7 @@ import javax.inject.Inject;
 @Model(adaptables = SlingHttpServletRequest.class,
     resourceType = Text.RESOURCE_TYPE,
     defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
 public final class Text implements com.adobe.cq.wcm.core.components.models.Text {
 
     public static final String RESOURCE_TYPE = "waters/components/content/text";
