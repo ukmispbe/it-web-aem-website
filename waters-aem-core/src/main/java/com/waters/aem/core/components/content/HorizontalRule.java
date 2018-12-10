06de2838299ed1efe5f3236e50e3aeeeacae8018
Mark Daugherty <mark.r.daugherty@gmail.com>
2018-12-10 11:58:36 -0600

    removed model annotation since component class is never instantiated.

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/HorizontalRule.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/HorizontalRule.java
@@ -1,13 +1,8 @@
 package com.waters.aem.core.components.content;
 
 import com.citytechinc.cq.component.annotations.Component;
-import com.icfolson.aem.library.core.components.AbstractComponent;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
-import org.apache.sling.models.annotations.Model;
 
 @Component(value = "Horizontal Rule")
-@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public final class HorizontalRule extends AbstractComponent {
+public final class HorizontalRule {
 
 }
