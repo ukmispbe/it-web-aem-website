c128672133df649dbe804fa415ce8febf92ec0e8
Nick LaPaglia <Nick.LaPaglia@icfi.com>
2018-12-12 10:16:51 -0600

    Made revisions to unit test based on PR comments

--- waters-aem-core/src/test/groovy/com/waters/aem/core/components/content/TextSpec.groovy
+++ waters-aem-core/src/test/groovy/com/waters/aem/core/components/content/TextSpec.groovy
@@ -6,37 +6,35 @@ import spock.lang.Unroll
 @Unroll
 class TextSpec extends AemLibraryModelSpec {
 
-    def setupSpec() {
-        pageBuilder.content {
-            waters {
-                one {
-                    "jcr:content" {
-                       text()
-                    }
-                }
-                two {
-                    "jcr:content" {
-                       text(
-                               title: "Introduction",
-                       )
-                    }
-                }
-            }
-        }
-    }
+	def setupSpec() {
+		pageBuilder.content {
+			waters {
+				one {
+					"jcr:content" {
+					   text()
+					}
+				}
+				two {
+					"jcr:content" {
+						text(
+                           title: "Introduction"
+						)
+					}
+				}
+			}
+		}
+	}
 
-    def "get title"() {
-        setup:
-        def textComponent = requestBuilder.setPath(path).build().adaptTo(Text)
+	def "get title"() {
+		setup:
+		def textComponent = requestBuilder.setPath(path).build().adaptTo(Text)
 
-        expect:
-        textComponent.title == title
+		expect:
+		textComponent.title == title
 
-        where:
-        path                                   | title
-        "/content/waters/one/jcr:content/text" | null
-        "/content/waters/two/jcr:content/text" | "Introduction"
-    }
-
-
+		where:
+		path                                   | title
+		"/content/waters/one/jcr:content/text" | null
+		"/content/waters/two/jcr:content/text" | "Introduction"
+	}
 }
