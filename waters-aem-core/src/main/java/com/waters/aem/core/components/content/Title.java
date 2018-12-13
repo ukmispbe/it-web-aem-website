012e72f57e1c00e2dbfae0b9cf4fbf0035db352d
Mark Daugherty <mark.r.daugherty@gmail.com>
2018-12-13 09:28:42 -0600

    updated title dialog config.

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/Title.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/Title.java
@@ -1,16 +1,72 @@
 package com.waters.aem.core.components.content;
 
 import com.citytechinc.cq.component.annotations.Component;
-import com.icfolson.aem.library.core.components.AbstractComponent;
+import com.citytechinc.cq.component.annotations.DialogField;
+import com.citytechinc.cq.component.annotations.HideDialogField;
+import com.citytechinc.cq.component.annotations.Property;
+import com.citytechinc.cq.component.annotations.Tab;
+import com.citytechinc.cq.component.annotations.widgets.RichTextEditor;
+import com.citytechinc.cq.component.annotations.widgets.rte.Format;
+import com.citytechinc.cq.component.annotations.widgets.rte.SubSuperscript;
+import com.day.cq.commons.jcr.JcrConstants;
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Via;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.via.ResourceSuperType;
+
+import javax.annotation.Nonnull;
 
 @Component(value = "Title",
     description = "Section Heading",
-    resourceSuperType = "core/wcm/components/title/v2/title",
-    editConfig = false)
-@Model(adaptables = SlingHttpServletRequest.class)
-public final class Title extends AbstractComponent {
+    resourceSuperType = Title.RESOURCE_SUPER_TYPE,
+    editConfig = false,
+    tabs = @Tab(title = "Properties", touchUINodeName = "properties"))
+@Model(adaptables = SlingHttpServletRequest.class,
+    resourceType = Title.RESOURCE_TYPE,
+    defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public final class Title implements com.adobe.cq.wcm.core.components.models.Title {
 
-    public static final String RESOURCE_TYPE = "waters/components/content/title";
+    static final String RESOURCE_SUPER_TYPE = "core/wcm/components/title/v2/title";
+
+    static final String RESOURCE_TYPE = "waters/components/content/title";
+
+    @Self
+    @Via(type = ResourceSuperType.class)
+    private com.adobe.cq.wcm.core.components.models.Title delegate; // delegate to core component class
+
+    @DialogField(fieldLabel = "Title",
+        fieldDescription = "Leave empty to use the page title.",
+        fieldName = "title",
+        name = "./" + JcrConstants.JCR_TITLE,
+        additionalProperties = {
+            @Property(name = "useFixedInlineToolbar", value = "true")
+        })
+    @RichTextEditor(
+        format = @Format(underline = false),
+        subsuperscript = @SubSuperscript
+    )
+    @Override
+    public String getText() {
+        return delegate.getText();
+    }
+
+    @DialogField
+    @HideDialogField
+    @Override
+    public String getLinkURL() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getType() {
+        return delegate.getType();
+    }
+
+    @Nonnull
+    @Override
+    public String getExportedType() {
+        return delegate.getExportedType();
+    }
 }
