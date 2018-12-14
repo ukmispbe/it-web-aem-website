7fb4b81fa01aa157aa8dba96fc9499934869a824
Mark Daugherty <mark.r.daugherty@gmail.com>
2018-12-13 20:41:31 -0600

    updated to use default coral3 multifield.

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/applicationnotes/ExternalArticles.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/applicationnotes/ExternalArticles.java
@@ -2,9 +2,9 @@ package com.waters.aem.core.components.content.applicationnotes;
 
 import com.citytechinc.cq.component.annotations.Component;
 import com.citytechinc.cq.component.annotations.DialogField;
+import com.citytechinc.cq.component.annotations.widgets.MultiField;
 import com.icfolson.aem.library.core.components.AbstractComponent;
 import com.icfolson.aem.library.models.annotations.InheritInject;
-import com.icfolson.aem.multicompositeaddon.widget.MultiCompositeField;
 import com.waters.aem.core.constants.WatersConstants;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
@@ -18,7 +18,7 @@ public class ExternalArticles extends AbstractComponent {
 
     @DialogField(fieldLabel = "Link Items",
         fieldDescription = "Enter external article details")
-    @MultiCompositeField
+    @MultiField(composite = true)
     @InheritInject
     private List<LinkItem> linkItems;
 
