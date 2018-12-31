82d4bac21a994aadb6892ddb12673377ca8067ca
Nick LaPaglia <Nick.LaPaglia@icfi.com>
2018-12-31 10:43:57 -0600

    Updated button component code

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/Button.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/Button.java
@@ -1,7 +1,10 @@
 package com.waters.aem.core.components.content;
 
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ExporterConstants;
 import com.citytechinc.cq.component.annotations.Component;
 import com.citytechinc.cq.component.annotations.DialogField;
+import com.citytechinc.cq.component.annotations.Tab;
 import com.citytechinc.cq.component.annotations.widgets.PathField;
 import com.citytechinc.cq.component.annotations.widgets.Switch;
 import com.citytechinc.cq.component.annotations.widgets.TextField;
@@ -11,13 +14,22 @@ import com.waters.aem.core.constants.WatersConstants;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
 
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
 
-@Component("Button")
+@Component(value = "Button",
+    description = "This is the Button component for Waters site",
+    tabs = @Tab(title = "Properties", touchUINodeName = "properties"))
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public final class Button {
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
+public final class Button implements ComponentExporter {
+
+    @Self
+    private Resource resource;
 
     @DialogField(fieldLabel = "Button Text",
         fieldDescription = "Enter the text for the button",
@@ -64,5 +76,11 @@ public final class Button {
 
     public Boolean isNewWindow() {
         return newWindow;
+    }
+
+    @Nonnull
+    @Override
+    public String getExportedType() {
+        return resource.getResourceType();
     }
 }
