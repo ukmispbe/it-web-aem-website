6f8241c0f7d10f295834c1735a344508c468d7fd
Mark Daugherty <mark.r.daugherty@gmail.com>
2019-01-08 17:28:00 -0600

    added base markup/js/styles for back to top button.

--- waters-aem-frontend/src/entry.js
+++ waters-aem-frontend/src/entry.js
@@ -6,6 +6,33 @@ import './styles/index.scss';
 var inlineSVG = require('inline-svg');
 
 inlineSVG.init({
-  svgSelector: 'img.inline-svg', // the class attached to all images that should be inlined
-  initClass: 'svg-inlined', // class added to <html>
-}, function () {});
+    svgSelector: 'img.inline-svg', // the class attached to all images that should be inlined
+    initClass: 'svg-inlined', // class added to <html>
+}, function () {
+
+});
+
+function trackScroll() {
+    var scrolled = window.pageYOffset;
+    var coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+        goTopBtn.classList.add('back-to-top-show');
+    }
+
+    if (scrolled < coords) {
+        goTopBtn.classList.remove('back-to-top-show');
+    }
+}
+
+function backToTop() {
+    if (window.pageYOffset > 0) {
+        window.scrollBy(0, -80);
+        setTimeout(backToTop, 0);
+    }
+}
+
+var goTopBtn = document.querySelector('.cmp-back-to-top');
+
+window.addEventListener('scroll', trackScroll);
+goTopBtn.addEventListener('click', backToTop);
