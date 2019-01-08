5ea4060c446d7cbc23fb4ad409a34c1476025e8e
Michael Nelson <Michael.Nelson@icfolson.com>
2019-01-08 10:44:45 -0600

    external header updates

--- waters-aem-frontend/src/entry.js
+++ waters-aem-frontend/src/entry.js
@@ -3,4 +3,11 @@
 import './styles/index.scss';
 
 // JS Code below
-console.log('search');
+var inlineSVG = require('inline-svg');
+
+inlineSVG.init({
+  svgSelector: 'img.inline-svg', // the class attached to all images that should be inlined
+  initClass: 'svg-inlined', // class added to <html>
+}, function () {
+  console.log('All SVGs inlined');
+});
