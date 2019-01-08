ecafde14ceb1ca8120465f124942a0b466fcebc3
Michael Nelson <Michael.Nelson@icfolson.com>
2019-01-08 13:14:17 -0600

    removing comment

--- waters-aem-frontend/src/entry.js
+++ waters-aem-frontend/src/entry.js
@@ -8,6 +8,4 @@ var inlineSVG = require('inline-svg');
 inlineSVG.init({
   svgSelector: 'img.inline-svg', // the class attached to all images that should be inlined
   initClass: 'svg-inlined', // class added to <html>
-}, function () {
-  console.log('All SVGs inlined');
-});
+}, function () {});
