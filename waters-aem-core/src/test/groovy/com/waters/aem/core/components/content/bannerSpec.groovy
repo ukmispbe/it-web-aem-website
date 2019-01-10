976373c05e08be7c00dedad627812e3f8765311f
Nick LaPaglia <Nick.LaPaglia@icfi.com>
2019-01-10 11:27:12 -0600

    Backend code and unit test for banner component

---
+++ waters-aem-core/src/test/groovy/com/waters/aem/core/components/content/bannerSpec.groovy
@@ -0,0 +1,41 @@
+package com.waters.aem.core.components.content
+
+import com.icfolson.aem.library.models.specs.AemLibraryModelSpec
+import spock.lang.Unroll
+
+@Unroll
+class bannerSpec extends AemLibraryModelSpec {
+
+    def setupSpec() {
+        pageBuilder.content {
+            waters {
+                one {
+                    "jcr:content" {
+                        banner()
+                    }
+                }
+                two {
+                    "jcr:content" {
+                        banner() {
+                            backgroundImage(fileReference: "/content/dam/waters/logo.png")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    def "get background image"() {
+        setup:
+        def banner = getResource(path).adaptTo(Banner)
+
+        expect:
+        banner.backgroundImage?.fileReference == fileReference
+
+        where:
+        path                                     | fileReference
+        "/content/waters/one/jcr:content/banner" | null
+        "/content/waters/two/jcr:content/banner" | "/content/dam/waters/logo.png"
+    }
+
+}
