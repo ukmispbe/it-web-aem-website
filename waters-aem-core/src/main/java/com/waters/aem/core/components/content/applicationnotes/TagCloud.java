7c3c827d6a195360eb8a174766536a0dbbe99aff
Mark Daugherty <mark.r.daugherty@gmail.com>
2019-01-16 13:12:45 -0600

    refactored tag component to include dialog fields on a model.

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/applicationnotes/TagCloud.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/applicationnotes/TagCloud.java
@@ -4,18 +4,12 @@ import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ExporterConstants;
 import com.citytechinc.cq.component.annotations.Component;
 import com.citytechinc.cq.component.annotations.DialogField;
-import com.citytechinc.cq.component.annotations.widgets.TagInputField;
+import com.citytechinc.cq.component.annotations.IncludeDialogFields;
 import com.citytechinc.cq.component.annotations.widgets.TextField;
-import com.day.cq.tagging.Tag;
-import com.day.cq.tagging.TagManager;
 import com.icfolson.aem.library.api.page.PageDecorator;
 import com.icfolson.aem.library.core.components.AbstractComponent;
-import com.icfolson.aem.library.models.annotations.TagInject;
-import com.waters.aem.core.components.content.PageMetaDataExtractor;
 import com.waters.aem.core.constants.WatersConstants;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
@@ -38,12 +32,9 @@ public final class TagCloud extends AbstractComponent implements ComponentExport
     @Inject
     private String title;
 
-    @DialogField(fieldLabel = "Tag Picker",
-        fieldDescription = "Select the Tags",
-        ranking = 2)
-    @TagInputField
-    @TagInject
-    private List<Tag> tags;
+    @IncludeDialogFields
+    @Self
+    private PageMetadata pageMetadata;
 
     @Self
     private Resource resource;
@@ -51,16 +42,11 @@ public final class TagCloud extends AbstractComponent implements ComponentExport
     @Inject
     private PageDecorator currentPage;
 
-    @Inject
-    private ResourceResolver resourceResolver;
-
-    public List<SearchFacet> getSearchFacets(){
-        final TagManager tagManager = resourceResolver.adaptTo(TagManager.class);
-        final ValueMap valueMap = currentPage.getProperties();
-        List<Tag> searchTags = PageMetaDataExtractor.getSearchTags(tagManager,valueMap,tags);
-       return searchTags.stream()
-                .map( tag -> new SearchFacet(tag.getTitle(currentPage.getLanguage(false)),tag.getParent().getName()))
-                .collect(Collectors.toList());
+    public List<SearchFacet> getSearchFacets() {
+        return pageMetadata.getSearchTags()
+            .stream()
+            .map(tag -> new SearchFacet(tag.getTitle(currentPage.getLanguage(false)), tag.getParent().getName()))
+            .collect(Collectors.toList());
     }
 
     public String getTitle() {
