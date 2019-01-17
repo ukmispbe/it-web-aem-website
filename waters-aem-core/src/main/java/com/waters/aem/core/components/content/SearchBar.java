70f000da2090feac941a05b781d045e32525e452
CORP\uslval <Lakshmi_valluripalli@waters.com>
2019-01-17 10:13:32 -0500

    BTWCT-41 Added Searbar form

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/SearchBar.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/SearchBar.java
@@ -6,7 +6,8 @@ import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 
-@Component("Search Bar")
+@Component(value = "Search Bar",
+        description = "This is the Search Bar component for Waters site")
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public final class SearchBar extends AbstractComponent {
 
