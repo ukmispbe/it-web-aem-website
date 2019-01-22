8afadce53ab2ef4402bbc97377261d073fa2ff7e
Michael Nelson <Michael.Nelson@icfolson.com>
2019-01-22 17:57:53 -0600

    Adding JS for scrolling and anchor nav

--- waters-aem-frontend/src/scripts/anchor.js
+++ waters-aem-frontend/src/scripts/anchor.js
@@ -1,32 +1,79 @@
-var  anchorList = document.querySelector('.cmp-anchor__list');
+import scrollToY from './scrollTo';
+var anchorList = document.querySelector('.cmp-anchor__list');
+var anchorElement = document.querySelector('.cmp-anchor');
 
-function anchorSticky() {
-var  anchor = document.querySelector(".cmp-anchor");
-anchorListItems = document.querySelector(".cmp-anchor__list");
-screenTop = document.querySelector('.cmp-anchor').getBoundingClientRect().top;
-if( document.documentElement.scrollTop  > screenTop ) {
-    anchor.addClass('sticky');
-    anchorListItems.addClass('cmp-anchor-hide');
-  } else {
-    anchor.removeClass('sticky');
-    anchorListItems.removeClass('cmp-anchor-hide');
-  }
-}
+// Setup click handler for Anchor Links to scroll in view
 
-function toggleMobileNav(){
-alert();
-if(anchorList.classList.contains('cmp-anchor-hide')){
-    anchorList.classList.add('cmp-anchor-show');
-    anchorList.classList.remove('cmp-anchor-hide');
-}
-else{
-    anchorList.classList.add('cmp-anchor-hide');
-    anchorList.classList.remove('cmp-anchor-show');
+var anchorLinks = document.querySelectorAll('.cmp-anchor__link');
+let current = undefined;
+anchorLinks.forEach((anchor, i) => {
+    try {
+        anchor.addEventListener('click', e => {
+            e.preventDefault();
+            anchorElement.classList.add('cmp-anchor--sticky');
+            setTimeout(() => {
+                const href = e.target.getAttribute('href').replace(/#/gi, '');
+                const block = document.getElementById(href);
+                const blockTop = block.offsetTop;
+
+                scrollToY(blockTop - 140, 1000, 'easeOutSine');
+            }, 0);
+        });
+    } catch (e) {
+        // ignore
     }
-//element.classList.contains("highlighted")
+});
+
+// Sticky Nav Component
+var anchorSticky = (function() {
+    var CSS_CLASS_ACTIVE = 'cmp-anchor--sticky';
+
+    var Sticky = {
+        element: null,
+        position: 0,
+        addEvents: function() {
+            window.addEventListener('scroll', this.onScroll.bind(this));
+        },
+        init: function(element) {
+            this.element = element;
+            this.addEvents();
+            this.position = element.offsetTop;
+            this.onScroll();
+        },
+        aboveScroll: function() {
+            return this.position - 73 < window.scrollY;
+        },
+        onScroll: function(event) {
+            if (this.aboveScroll()) {
+                this.setFixed();
+            } else {
+                this.setRelative();
+            }
+        },
+        setFixed: function() {
+            this.element.classList.add(CSS_CLASS_ACTIVE);
+        },
+        setRelative: function() {
+            this.element.classList.remove(CSS_CLASS_ACTIVE);
+        },
+    };
+
+    return Sticky;
+})();
+
+//  Init Sticky
+
+if (anchorElement) anchorSticky.init(anchorElement);
+
+function toggleMobileNav() {
+    if (anchorList.classList.contains('cmp-anchor-hide')) {
+        anchorList.classList.add('cmp-anchor-show');
+        anchorList.classList.remove('cmp-anchor-hide');
+    } else {
+        anchorList.classList.add('cmp-anchor-hide');
+        anchorList.classList.remove('cmp-anchor-show');
+    }
+    //element.classList.contains("highlighted")
 }
 
-window.addEventListener('scroll', anchorSticky)
-anchorList.addEventListener('click', () => {
-    toggleMobileNav()
-})
+window.addEventListener('scroll', anchorSticky);
