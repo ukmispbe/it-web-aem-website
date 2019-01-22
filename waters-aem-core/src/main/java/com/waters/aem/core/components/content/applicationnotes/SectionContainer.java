21fd6596a0817fb7202917a844962a8dd6b40ba6
Mark Daugherty <mark.r.daugherty@gmail.com>
2019-01-22 11:16:08 -0600

    added container exporter for section container model.

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/applicationnotes/SectionContainer.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/applicationnotes/SectionContainer.java
@@ -1,5 +1,9 @@
 package com.waters.aem.core.components.content.applicationnotes;
 
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ContainerExporter;
+import com.adobe.cq.export.json.ExporterConstants;
+import com.adobe.cq.export.json.SlingModelFilter;
 import com.citytechinc.cq.component.annotations.Component;
 import com.citytechinc.cq.component.annotations.DialogField;
 import com.citytechinc.cq.component.annotations.Listener;
@@ -9,11 +13,21 @@ import com.icfolson.aem.library.api.link.Link;
 import com.icfolson.aem.library.core.components.AbstractComponent;
 import com.icfolson.aem.library.core.link.builders.factory.LinkBuilderFactory;
 import com.waters.aem.core.constants.WatersConstants;
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.factory.ModelFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static com.icfolson.aem.library.core.constants.ComponentConstants.EVENT_AFTER_DELETE;
 import static com.icfolson.aem.library.core.constants.ComponentConstants.EVENT_AFTER_EDIT;
@@ -31,10 +45,30 @@ import static com.icfolson.aem.library.core.constants.ComponentConstants.REFRESH
         @Listener(name = EVENT_AFTER_EDIT, value = REFRESH_PARENT),
         @Listener(name = EVENT_AFTER_DELETE, value = REFRESH_PARENT)
     })
-@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public final class SectionContainer extends AbstractComponent {
+@Model(adaptables = SlingHttpServletRequest.class,
+    adapters = { SectionContainer.class, ContainerExporter.class },
+    resourceType = SectionContainer.RESOURCE_TYPE,
+    defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
+public final class SectionContainer extends AbstractComponent implements ContainerExporter {
 
     public static final String RESOURCE_TYPE = "waters/components/content/applicationnotes/sectioncontainer";
+
+    private static final Logger LOG = LoggerFactory.getLogger(SectionContainer.class);
+
+    @Inject
+    private SlingModelFilter slingModelFilter;
+
+    @OSGiService
+    private ModelFactory modelFactory;
+
+    @Self
+    private SlingHttpServletRequest request;
+
+    @Inject
+    private Resource resource;
+
+    private Map<String, ComponentExporter> exportedItems;
 
     @DialogField(fieldLabel = "Title",
         required = true,
@@ -52,5 +86,38 @@ public final class SectionContainer extends AbstractComponent {
         return LinkBuilderFactory.forPath("#" + getId())
             .setTitle(getTitle())
             .build();
+    }
+
+    @Nonnull
+    @Override
+    public Map<String, ? extends ComponentExporter> getExportedItems() {
+        if (exportedItems == null) {
+            exportedItems = new LinkedHashMap<>();
+
+            for (final Resource child : slingModelFilter.filterChildResources(resource.getChildren())) {
+                final ComponentExporter model = modelFactory.getModelFromWrappedRequest(request, child,
+                    ComponentExporter.class);
+
+                if (model != null) {
+                    exportedItems.put(child.getName(), model);
+                }
+            }
+        }
+
+        LOG.info("found {} exported items", exportedItems.size());
+
+        return exportedItems;
+    }
+
+    @Nonnull
+    @Override
+    public String[] getExportedItemsOrder() {
+        return getExportedItems().keySet().toArray(new String[0]);
+    }
+
+    @Nonnull
+    @Override
+    public String getExportedType() {
+        return RESOURCE_TYPE;
     }
 }
