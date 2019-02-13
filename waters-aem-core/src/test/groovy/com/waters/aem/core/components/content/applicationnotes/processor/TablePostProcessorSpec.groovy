52b83fac4c8ed958ff16b18cf670e63dd9cb6948
Mark Daugherty <mark.r.daugherty@gmail.com>
2019-02-13 16:59:49 -0600

    table updates for translation.

---
+++ waters-aem-core/src/test/groovy/com/waters/aem/core/components/content/applicationnotes/processor/TablePostProcessorSpec.groovy
@@ -0,0 +1,68 @@
+package com.waters.aem.core.components.content.applicationnotes.processor
+
+import com.icfolson.aem.library.models.specs.AemLibraryModelSpec
+import com.waters.aem.core.components.content.applicationnotes.Table
+import com.waters.aem.core.services.ExcelTableParser
+import com.waters.aem.core.services.impl.POIExcelTableParser
+import spock.lang.Shared
+import spock.lang.Unroll
+
+@Unroll
+class TablePostProcessorSpec extends AemLibraryModelSpec {
+
+    private static final Map<String, String> TABLE_JSON_MAP = [
+        "/content/waters/table/jcr:content/table-with-header": "[{\"0\":[\"One\"],\"1\":[\"Two\"]},{\"0\":[\"LC " +
+            "system:\"],\"1\":[\" ACQUITY UPLC H-Class Bio\"]},{\"0\":[\"Detection:\"],\"1\":[\" ACQUITY UPLC TUV " +
+            "Detector with 5 mm titanium flow cell\"]},{\"0\":[\"Wavelength:\"],\"1\":[\" 214 nm\"]}," +
+            "{\"0\":[\"Columns:\"],\"1\":[\" ACQUITY UPLC Peptide BEH C18, 130., 1.7 µm, 2.1 x 150 mm (p/n  " +
+            "186003556)\"]}]",
+        "/content/waters/table/jcr:content/table-no-header": "[{\"0\":[\"LC system:\"]," +
+            "\"1\":[\" ACQUITY UPLC H-Class Bio\"]},{\"0\":[\"Detection:\"],\"1\":[\" ACQUITY UPLC TUV Detector with " +
+            "5 mm titanium flow cell\"]},{\"0\":[\"Wavelength:\"],\"1\":[\" 214 nm\"]},{\"0\":[\"Columns:\"]," +
+            "\"1\":[\" ACQUITY UPLC Peptide BEH C18, 130., 1.7 µm, 2.1 x 150 mm (p/n  186003556)\"]}]"
+    ]
+
+    @Shared
+    TablePostProcessor processor
+
+    def setupSpec() {
+        slingContext.registerService(ExcelTableParser, new POIExcelTableParser())
+        slingContext.addModelsForClasses(Table)
+
+        processor = slingContext.registerInjectActivateService(new TablePostProcessor())
+    }
+
+    def "persist empty list JSON when table excel file is null"() {
+        setup:
+        def resourcePath = "/content/waters/table/jcr:content/table-empty"
+
+        def request = requestBuilder.build {
+            path = resourcePath
+        }
+
+        when:
+        processor.process(request, [])
+
+        then:
+        getResource(resourcePath).valueMap.get(Table.PROPERTY_TABLE_ROWS_JSON, "") == "[]"
+    }
+
+    def "persist JSON for table excel file"() {
+        setup:
+        def request = requestBuilder.build {
+            path = resourcePath
+        }
+
+        when:
+        processor.process(request, [])
+
+        then:
+        getResource(resourcePath).valueMap.get(Table.PROPERTY_TABLE_ROWS_JSON, "") == TABLE_JSON_MAP[resourcePath]
+
+        where:
+        resourcePath << [
+            "/content/waters/table/jcr:content/table-with-header",
+            "/content/waters/table/jcr:content/table-no-header"
+        ]
+    }
+}
