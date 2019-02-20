40c25ee636f0eadd8e28c95a186cfe70a5c8a3b5
CORP\uslval <Lakshmi_valluripalli@waters.com>
2019-02-20 11:03:57 -0500

    changes class to className

--- waters-aem-frontend/src/search/components/tagcloud.js
+++ waters-aem-frontend/src/search/components/tagcloud.js
@@ -13,12 +13,12 @@ class TagCloud extends Component {
 
         const mappedResults = this.props.keywords.map((keyword, i) => {
                     let boundItemClick = this.handleRelatedSearch.bind(this, keyword.filter);
-                    return (<li class="cmp-tag-cloud__item" key={i}><a onClick={boundItemClick}>{keyword.title}</a></li>);
+                    return (<li className="cmp-tag-cloud__item" key={i}><a onClick={boundItemClick}>{keyword.title}</a></li>);
                     });
         return ([
 
             <h3>{this.props.tagCloudTitle}</h3>,
-            <ul class="cmp-tag-cloud__list">
+            <ul className="cmp-tag-cloud__list">
                  {mappedResults}
             </ul>
         ]);
