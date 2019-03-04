5ad618b619c5f8cde0a5af409ea055fffeb83899
Mark Daugherty <mark.r.daugherty@gmail.com>
2019-03-04 13:27:22 -0600

    added data attribute to expose asset metadata for button.

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/Button.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/Button.java
@@ -8,9 +8,13 @@ import com.citytechinc.cq.component.annotations.Tab;
 import com.citytechinc.cq.component.annotations.widgets.PathField;
 import com.citytechinc.cq.component.annotations.widgets.Switch;
 import com.citytechinc.cq.component.annotations.widgets.TextField;
+import com.day.cq.dam.api.Asset;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.icfolson.aem.library.api.link.Link;
+import com.icfolson.aem.library.core.constants.PathConstants;
 import com.icfolson.aem.library.models.annotations.LinkInject;
-import com.waters.aem.core.constants.WatersConstants;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
@@ -20,6 +24,8 @@ import org.apache.sling.models.annotations.injectorspecific.Self;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component(value = "Button",
     description = "This is the Button component for Waters site",
@@ -27,6 +33,8 @@ import javax.inject.Inject;
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
 public final class Button implements ComponentExporter {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper().configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, true);
 
     @Self
     private Resource resource;
@@ -50,7 +58,7 @@ public final class Button implements ComponentExporter {
         fieldDescription = "Select or enter the link URL",
         required = true,
         ranking = 3)
-    @PathField(rootPath = WatersConstants.ROOT_PATH)
+    @PathField(rootPath = PathConstants.PATH_CONTENT)
     @LinkInject
     private Link buttonLink;
 
@@ -61,6 +69,8 @@ public final class Button implements ComponentExporter {
     @Inject
     @Default(booleanValues = false)
     private Boolean newWindow;
+
+    private Map<String, Object> assetMetadata;
 
     public String getButtonText() {
         return buttonText;
@@ -78,9 +88,34 @@ public final class Button implements ComponentExporter {
         return newWindow;
     }
 
+    public String getAssetMetadataJson() throws JsonProcessingException {
+
+        return MAPPER.writeValueAsString(getAssetMetadata());
+    }
+
     @Nonnull
     @Override
     public String getExportedType() {
         return resource.getResourceType();
+    }
+
+    private Map<String, Object> getAssetMetadata() {
+        if (assetMetadata == null) {
+            assetMetadata = new HashMap<>();
+
+            if (isAsset()) {
+                final Resource assetResource = resource.getResourceResolver().getResource(buttonLink.getPath());
+
+                if (assetResource != null) {
+                    assetMetadata = assetResource.adaptTo(Asset.class).getMetadata();
+                }
+            }
+        }
+
+        return assetMetadata;
+    }
+
+    private Boolean isAsset() {
+        return buttonLink != null && buttonLink.getPath().startsWith(PathConstants.PATH_CONTENT_DAM);
     }
 }
