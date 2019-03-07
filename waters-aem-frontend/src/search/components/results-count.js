6c17c0ea130893ffd92fe2b91791e9c51f41228d
Danny Ha <danny.ha@icfolson.com>
2019-03-07 10:36:35 -0500

    BTWCT-375: styles updates and added ellipsis to search word when over 120 characters

--- waters-aem-frontend/src/search/components/results-count.js
+++ waters-aem-frontend/src/search/components/results-count.js
@@ -3,11 +3,13 @@ import React from 'react';
 const ResultsCount = ({ current, rows, count, query, noQuery }) => {
     const endResults = count > current * rows ? current * rows : count;
     const startResults = current * rows - rows + 1;
+    const maxLength = 120;
+    const thisQuery = (query && query.toString().length > maxLength) ? query.substring(0,maxLength) + '...' : query;
     const forQuery = (
         <span>
             for{' '}
             <strong>
-                <q className="query">{query}</q>
+                <q className="query">{thisQuery}</q>
             </strong>
         </span>
     );
