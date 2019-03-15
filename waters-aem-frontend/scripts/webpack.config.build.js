ea563eb07d62c4bdd33706c01948d7a077647723
Mark Daugherty <mark.r.daugherty@gmail.com>
2019-03-15 11:39:38 -0500

    moved grid to separate clientlib.

--- waters-aem-frontend/scripts/webpack.config.build.js
+++ waters-aem-frontend/scripts/webpack.config.build.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/entry.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, '../', 'build'),
+        path: path.resolve(__dirname, '../', 'build')
     },
     module: {
         rules: [
@@ -13,30 +13,30 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
                 use: {
-                    loader: 'babel-loader',
-                },
+                    loader: 'babel-loader'
+                }
             },
             {
                 test: /\.scss$/,
                 use: [
                     {
                         // loader: 'style-loader'
-                        loader: MiniCssExtractPlugin.loader,
+                        loader: MiniCssExtractPlugin.loader
                     },
                     {
-                        loader: 'css-loader',
+                        loader: 'css-loader'
                     },
                     {
-                        loader: 'sass-loader',
-                    },
-                ],
-            },
-        ],
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
             filename: '[name].css',
             chunkFilename: '[id].css',
-        }),
-    ],
+        })
+    ]
 };
