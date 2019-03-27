a9e40a1c3e566ec37fa3adf79f0f60881ec72220
LaPaglia <nick.lapaglia@icfolson.com>
2019-03-27 10:04:18 -0500

    Created script to change category tag from applicationslibrary to library

---
+++ waters-aem-ui.apps/src/main/scripts/groovy/ChangeCategoryTagName.groovy
@@ -0,0 +1,33 @@
+final def page = getPage('/content/waters/language-masters/en/application-notes')
+final def query = buildQuery(page)
+final def result = query.execute()
+
+def buildQuery(page){
+    def queryManager = session.workspace.queryManager
+    def queryString = "SELECT * FROM [nt:base] WHERE ISDESCENDANTNODE([$page.path])"
+    queryManager.createQuery(queryString, "JCR-SQL2")
+}
+
+result.nodes.each { node ->
+
+    node.findAll { it.hasProperties() }.each { nonEmptyNode ->
+
+        nonEmptyNode.getProperties().findAll {it.isMultiple() && it.getType() == 1 }.each { prop ->
+
+            def valueList = []
+
+            prop.getValues().findAll { it.getString().equals('waters:category/applicationslibrary') }.each { val ->
+                val.getString().equals('waters:category/applicationslibrary') ? valueList.push('waters:category/library') : valueList.push(val.getString())
+            }
+
+            if(valueList.size() > 0){
+                nonEmptyNode.setProperty(prop.getName(), valueList as String[])
+            }
+
+        }
+
+        session.save()
+    }
+}
+
+
