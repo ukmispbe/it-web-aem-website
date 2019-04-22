83da08ced874247ec5bcb7b781152a9021375c4f
Mark Daugherty <mark.r.daugherty@gmail.com>
2019-04-22 11:58:31 -0500

    test updates.

--- waters-aem-ui.automation-tests/src/main/java/com/waters/aem/automationtests/modules/WatersComponentModule.java
+++ waters-aem-ui.automation-tests/src/main/java/com/waters/aem/automationtests/modules/WatersComponentModule.java
@@ -1,8 +1,10 @@
 package com.waters.aem.automationtests.modules;
 
+import com.adobe.cq.wcm.core.components.models.Title;
 import com.google.inject.AbstractModule;
 import com.waters.aem.automationtests.components.Text;
 import com.waters.aem.automationtests.components.impl.TextImpl;
+import com.waters.aem.automationtests.components.impl.TitleImpl;
 
 /**
  * Module for Waters components.
@@ -12,5 +14,6 @@ public class WatersComponentModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(Text.class).to(TextImpl.class);
+        bind(Title.class).to(TitleImpl.class);
     }
 }
--- waters-aem-ui.automation-tests/src/test/java/com/waters/aem/automationtests/components/TitleComponentTest.java
+++ waters-aem-ui.automation-tests/src/test/java/com/waters/aem/automationtests/components/TitleComponentTest.java
@@ -1,5 +1,6 @@
 package com.waters.aem.automationtests.components;
 
+import com.adobe.cq.wcm.core.components.models.Title;
 import com.cognifide.qa.bb.aem.core.api.AemActions;
 import com.cognifide.qa.bb.aem.core.component.actions.ConfigureComponentData;
 import com.cognifide.qa.bb.aem.core.component.configuration.ResourceFileLocation;
@@ -15,20 +16,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Modules(BobcatRunModule.class)
 @Epic("Waters Automation Tests")
-@Feature("Text Component")
-public class TextComponentTest extends AbstractApplicationNotesPageTest {
+@Feature("Title Component")
+public class TitleComponentTest extends AbstractApplicationNotesPageTest {
 
     @Override
     public void configureComponent() throws ActionException {
-        controller.execute(AemActions.CONFIGURE_COMPONENT, new ConfigureComponentData("container", "Text", 0,
-            new ResourceFileLocation("text.yaml")));
+        controller.execute(AemActions.CONFIGURE_COMPONENT, new ConfigureComponentData("container", "Title", 0,
+            new ResourceFileLocation("title.yaml")));
     }
 
     @Test
-    public void getTitleAndText() {
-        final Text textComponent = applicationNotesPage.getContent(Text.class, 0);
+    public void getTitleAndType() {
+        final Title titleComponent = applicationNotesPage.getContent(Title.class, 0);
 
-        assertThat(textComponent.getTitle().equals("Title Test"));
-        assertThat(textComponent.getText().equals("Text Test"));
+        assertThat(titleComponent.getText().equals("Title Test"));
+        assertThat(titleComponent.getType().equals("h2"));
     }
 }
