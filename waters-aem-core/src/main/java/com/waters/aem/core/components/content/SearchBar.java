9803de8a7b2a1d98fbc7bb5b0db54d2bc10af33e
LaPaglia <nick.lapaglia@icfolson.com>
2019-04-25 09:59:12 -0500

    Added method to get base urlin search bar component

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/SearchBar.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/SearchBar.java
@@ -1,9 +1,28 @@
 package com.waters.aem.core.components.content;
 
 import com.citytechinc.cq.component.annotations.Component;
+import com.waters.aem.core.services.solr.SolrSearchService;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component(value = "Search Bar",
     description = "This is the Search Bar component for Waters site")
+@Model(adaptables = SlingHttpServletRequest.class,
+    defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public final class SearchBar {
     // placeholder for component plugin, not used by template
+
+    Logger log = LoggerFactory.getLogger(SearchBar.class);
+
+    @OSGiService
+    private SolrSearchService searchService;
+
+    public String getBaseUrl() {
+        log.info("Base Url = {}", searchService.getBaseUrl());
+        return searchService.getBaseUrl();
+    }
 }
