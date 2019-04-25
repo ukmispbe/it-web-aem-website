4c7d7ff898346b6ec99507f6aee8c3a49dc50531
LaPaglia <nick.lapaglia@icfolson.com>
2019-04-25 10:00:46 -0500

    Removed logging from search bar

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/SearchBar.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/SearchBar.java
@@ -6,8 +6,6 @@ import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Component(value = "Search Bar",
     description = "This is the Search Bar component for Waters site")
@@ -16,13 +14,10 @@ import org.slf4j.LoggerFactory;
 public final class SearchBar {
     // placeholder for component plugin, not used by template
 
-    Logger log = LoggerFactory.getLogger(SearchBar.class);
-
     @OSGiService
     private SolrSearchService searchService;
 
     public String getBaseUrl() {
-        log.info("Base Url = {}", searchService.getBaseUrl());
         return searchService.getBaseUrl();
     }
 }
