19438a0c3299ba4bf39d8b79cf1e64f4466bc4aa
Mark Daugherty <mark.r.daugherty@gmail.com>
2019-05-10 14:32:22 -0500

    added javadocs.

--- waters-aem-hybris-importer/src/main/java/com/waters/aem/hybris/executor/HybrisImporterExecutorService.java
+++ waters-aem-hybris-importer/src/main/java/com/waters/aem/hybris/executor/HybrisImporterExecutorService.java
@@ -3,11 +3,29 @@ package com.waters.aem.hybris.executor;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.waters.aem.hybris.result.HybrisImporterExecutionResult;
 
+/**
+ * Executor service for the Hybris importer process.
+ */
 public interface HybrisImporterExecutorService {
 
+    /**
+     * Execute a Hybris import.
+     *
+     * @return future containing the result of the asynchronous import process
+     */
     ListenableFuture<HybrisImporterExecutionResult> execute();
 
+    /**
+     * Get the result of the last Hybris import.
+     *
+     * @return future containing the result of the asynchronous import process
+     */
     ListenableFuture<HybrisImporterExecutionResult> getResult();
 
+    /**
+     * Determine if an import process is running.
+     *
+     * @return true if running
+     */
     Boolean isRunning();
 }
