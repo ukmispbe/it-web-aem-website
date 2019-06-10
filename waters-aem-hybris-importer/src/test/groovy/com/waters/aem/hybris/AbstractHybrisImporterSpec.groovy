f5064496ae03736457c04de9387ad79d465757e3
Mark Daugherty <mark.r.daugherty@gmail.com>
2019-06-10 16:00:11 -0500

    test fix.

--- waters-aem-hybris-importer/src/test/groovy/com/waters/aem/hybris/AbstractHybrisImporterSpec.groovy
+++ waters-aem-hybris-importer/src/test/groovy/com/waters/aem/hybris/AbstractHybrisImporterSpec.groovy
@@ -3,6 +3,8 @@ package com.waters.aem.hybris
 import com.icfolson.aem.prosper.specs.ProsperSpec
 import com.waters.aem.core.commerce.services.SkuRepository
 import com.waters.aem.core.commerce.services.impl.DefaultSkuRepository
+import com.waters.aem.core.services.SiteRepository
+import com.waters.aem.core.services.impl.DefaultSiteRepository
 import com.waters.aem.hybris.client.HybrisClient
 import com.waters.aem.hybris.client.MockHybrisClient
 import com.waters.aem.hybris.importer.HybrisCatalogImporter
@@ -17,6 +19,7 @@ abstract class AbstractHybrisImporterSpec extends ProsperSpec {
     def setupSpec() {
         slingContext.registerService(HybrisClient, new MockHybrisClient())
         slingContext.registerService(SkuRepository, new DefaultSkuRepository())
+        slingContext.registerService(SiteRepository, new DefaultSiteRepository())
 
         hybrisCatalogImporter = slingContext.registerInjectActivateService(new DefaultHybrisCatalogImporter())
     }
