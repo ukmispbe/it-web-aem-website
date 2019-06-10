f406fff169fdf9700044c381a62c209d86a61f1f
Garcia, Josue <josue.garcia@icfnext.com>
2019-06-10 11:47:43 -0500

    BTWCT-850: Autosuggestion is not working on test => fix autosuggestion API url initialization

--- waters-aem-frontend/src/search/components/searchbar.js
+++ waters-aem-frontend/src/search/components/searchbar.js
@@ -19,9 +19,7 @@ class SearchBar extends Component {
 
         this.searchBarRef = React.createRef();
 
-        this.search = new SearchService(
-            this.props.isocode,
-            this.props.searchServicePath,'', this.props.baseUrl);
+        this.search = new SearchService(this.props.isocode, this.props.baseUrl);
 
         let searchValue = this.search.getUrlParameter('keyword', window.location.search.substring(1)); 
 
