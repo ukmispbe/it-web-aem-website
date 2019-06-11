a607412fab415165b2e375ae1c81c3e6bd2eac1a
ukmispbe <Paul_Beard@waters.com>
2019-06-11 11:15:12 +0100

    BTWCT-762 Modified the Breadcrumb to include Search and Breadcrumb if accessed from Search Page

--- waters-aem-frontend/src/scripts/backtosearch.js
+++ waters-aem-frontend/src/scripts/backtosearch.js
@@ -19,14 +19,15 @@ function checkForSessionLink() {
 }
 
 function hideBreadcrumbShowBackToSearch(link) {
-    const breadcrumb = document.querySelectorAll('.cmp-breadcrumb');
+    //const breadcrumb = document.querySelectorAll('.cmp-breadcrumb');
     const searchButton = document.querySelectorAll('.cmp-breadcrumb-search');
 
-    for (let i = 0; i < breadcrumb.length; i++) {
-        const crumb = breadcrumb[i];
+    // Breadcrumb not hidden now
+    // for (let i = 0; i < breadcrumb.length; i++) {
+    //     const crumb = breadcrumb[i];
 
-        crumb.classList.add('cmp-breadcrumb--disable');
-    }
+    //     crumb.classList.add('cmp-breadcrumb--disable');
+    // }
 
     for (let n = 0; n < searchButton.length; n++) {
         const button = searchButton[n];
@@ -40,8 +41,55 @@ function hideBreadcrumbShowBackToSearch(link) {
         });
     }
 }
+
+function clearRHSGradients() {
+    var listItems = document.querySelectorAll('.cmp-breadcrumb__item');
+    for (var i = 0; i < listItems.length; i++) {
+        listItems[i].classList.remove("rhs-gradient-fade");
+    }
+}
+
+var mediaQueryListener = window.matchMedia('(max-width: 650px)');
+
+function changeBreadcrumb(source) {
+    var scrollOffset = 40;
+    if (source === 'FromScroll') {
+        scrollOffset = 10;
+    }
+    if (mediaQueryListener.matches) {
+        clearRHSGradients();
+
+        var breadcrumbContainer = document.querySelector('.breadcrumb');
+
+        var breadcrumbContainerWidth = breadcrumbContainer.clientWidth;
+        // Find the first Breadcrumb item that's not fully visible
+        var listItems = document.querySelectorAll('.cmp-breadcrumb__item');
+        for (var i = 0; i < listItems.length; i++) {
+            var rect = listItems[i].getBoundingClientRect();
+            var rhsItem = rect.left + rect.width;
+            if (breadcrumbContainerWidth < rhsItem + scrollOffset) {
+                if (breadcrumbContainer.clientWidth + breadcrumbContainer.scrollLeft < breadcrumbContainer.scrollWidth - 3) {
+                    listItems[i].classList.add("rhs-gradient-fade");
+                }
+                break;
+            }
+
+        }
+    }
+}
+
+mediaQueryListener.addListener(changeBreadcrumb);
+
+var breadcrumbDiv = document.querySelector('.breadcrumb');
+breadcrumbDiv.addEventListener('scroll', () => changeBreadcrumb('FromScroll'));
+
 const link = checkForSessionLink();
 
 if (link) {
     hideBreadcrumbShowBackToSearch(link);
 }
+
+// PB Test code
+//hideBreadcrumbShowBackToSearch("http://localhost:4502/language-masters/en/search.html?keyword=amino%20acid&sort=most-relevant");
+
+
