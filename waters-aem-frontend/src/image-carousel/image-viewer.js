ccf4c857a026e033088c87e4d87f2e7db591c350
Garcia, Josue <josue.garcia@icfnext.com>
2019-06-18 09:50:42 -0500

    BTWCT-839: Image Gallery Component => install react-alice-carousel and implement with image viewer

--- waters-aem-frontend/src/image-carousel/image-viewer.js
+++ waters-aem-frontend/src/image-carousel/image-viewer.js
@@ -16,85 +16,9 @@ class ImageViewer extends React.Component {
         this.containerRef = React.createRef();
         this.figureRef = React.createRef();
     }
-
-    componentDidMount() {
-        this.calculateWidth();
-        
-        // delay so the figure element contains the calculated image
-        setTimeout(() => this.setStateImageHeight(), 500);
-
-        window.addEventListener("resize", this.calculateWidth);
-
-        // this is for iPad orientation
-        window.addEventListener("orientationchange", this.calculateWidth);
-
-        // this is for mobile devices
-        window.addEventListener('deviceorientation', this.calculateWidth);
-      }
     
     handleOnDragStart = e => e.preventDefault();
 
-    renderZoomIcon = () => (this.state.magnified ? "-" : "+");
-
-    render() {
-        return <div ref={this.containerRef} className="image-viewer-container">
-        <div className="image-viewer-container__image-display">
-          <figure
-            onDragStart={this.handleOnDragStart}
-            ref={this.figureRef}
-            className={`image-viewer-container__image-figure image-viewer-container__image-figure--${this.state.magnified}`}
-            style={this.getFigureStyle()}
-            onMouseMove={this.handleFigureMouseMove}
-            onTouchStart={this.handleFigureTouchStart}
-            onTouchEnd={this.handleFigureTouchEnd}
-            onTouchMove={this.handleFigureTouchMove}>
-            <img
-              className="image-viewer-container__image-element"
-              src={this.state.imageSrc}
-              alt=""
-            />
-          </figure>
-          <div
-            className="image-viewer-container__image-zoom"
-            onClick={this.handleMagnifyClick}>
-            <div>{this.renderZoomIcon()}</div>
-          </div>
-        </div>
-      </div>
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.calculateWidth);
-        window.removeEventListener("orientationchange", this.calculateWidth);
-        window.removeEventListener("deviceorientation", this.calculateWidth);
-    }
-
-    getClosestWidth = containerWidth => this.props.widths.reduce((prev, curr) => curr > containerWidth ? prev : curr);
-
-    calculateWidth = () => {
-        const containerWidth = this.containerRef.current.getBoundingClientRect().width;
-        const imageWidth = this.getClosestWidth(containerWidth);
-        const imageSrc = this.props.template.replace(/{{width}}/gi, imageWidth);
-
-        this.setState({
-            containerWidth,
-            imageWidth,
-            imageSrc
-        });
-
-        if (this.props.onCalculate) {
-            setTimeout(() => {
-                this.setStateImageHeight();
-                this.props.onCalculate(this.state), 500
-            });
-        }
-    };
-
-    setStateImageHeight = () => {
-        const imageHeight = this.figureRef.current.getBoundingClientRect().height;
-        this.setState({imageHeight});
-    }
-    
     handleMagnifyClick = () => {
         this.figureRef.current.style.backgroundPosition = "";
 
@@ -110,18 +34,7 @@ class ImageViewer extends React.Component {
 
         this.setState({ magnified });
     };
-    
-    getFigureStyle = () => 
-        this.state.magnified
-        ? {
-            backgroundImage: `url(${this.state.imageSrc})`,
-            backgroundSize: `${this.state.imageWidth * 2}px ${this.state.imageHeight * 2}px`,
-            maxWidth: `${this.state.imageWidth}px`
-        }
-        : {
-            maxWidth: `${this.state.imageWidth}px`
-        };
-    
+
     handleFigureMove = (magnified, offsetX, offsetY, figureElement) => {
         if (!magnified) return;
 
@@ -166,6 +79,92 @@ class ImageViewer extends React.Component {
             document.body.classList.remove("lock-scroll");
         }
     };
+
+    render() {
+        return <div ref={this.containerRef} className="image-viewer-container">
+        <div className="image-viewer-container__image-display">
+          <figure
+            onDragStart={this.handleOnDragStart}
+            ref={this.figureRef}
+            className={`image-viewer-container__image-figure image-viewer-container__image-figure--${this.state.magnified}`}
+            style={this.getFigureStyle()}
+            onMouseMove={this.handleFigureMouseMove}
+            onTouchStart={this.handleFigureTouchStart}
+            onTouchEnd={this.handleFigureTouchEnd}
+            onTouchMove={this.handleFigureTouchMove}>
+            <img
+            className="image-viewer-container__image-element"
+            src={this.state.imageSrc}
+            alt=""/>
+          </figure>
+          <div
+            className="image-viewer-container__image-zoom"
+            onClick={this.handleMagnifyClick}>
+            <div>{this.renderZoomIcon()}</div>
+          </div>
+        </div>
+      </div>
+    }
+
+    componentDidMount() {
+        this.calculateWidth();
+
+        // delay so the figure element contains the calculated image
+        setTimeout(() => this.setStateImageHeight(), 500);
+
+        window.addEventListener("resize", this.calculateWidth);
+
+        // this is for iPad orientation
+        window.addEventListener("orientationchange", this.calculateWidth);
+
+        // this is for mobile devices
+        window.addEventListener('deviceorientation', this.calculateWidth);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.calculateWidth);
+        window.removeEventListener("orientationchange", this.calculateWidth);
+        window.removeEventListener("deviceorientation", this.calculateWidth);
+    }
+
+    renderZoomIcon = () => (this.state.magnified ? "-" : "+");
+
+    getClosestWidth = containerWidth => this.props.widths.reduce((prev, curr) => curr > containerWidth ? prev : curr);
+
+    calculateWidth = () => {
+        const containerWidth = this.containerRef.current.getBoundingClientRect().width;
+        const imageWidth = this.getClosestWidth(containerWidth);
+        const imageSrc = this.props.template.replace(/{{width}}/gi, imageWidth);
+
+        this.setState({
+            containerWidth,
+            imageWidth,
+            imageSrc
+        });
+
+        if (this.props.onCalculate) {
+            setTimeout(() => {
+                this.setStateImageHeight();
+                this.props.onCalculate(this.state), 500
+            });
+        }
+    };
+
+    setStateImageHeight = () => {
+        const imageHeight = this.figureRef.current.getBoundingClientRect().height;
+        this.setState({imageHeight});
+    }
+    
+    getFigureStyle = () => 
+        this.state.magnified
+        ? {
+            backgroundImage: `url(${this.state.imageSrc})`,
+            backgroundSize: `${this.state.imageWidth * 2}px ${this.state.imageHeight * 2}px`,
+            maxWidth: `${this.state.imageWidth}px`
+        }
+        : {
+            maxWidth: `${this.state.imageWidth}px`
+        };
 }
 
 ImageViewer.propTypes = {
--- waters-aem-frontend/src/image-carousel/index.js
+++ waters-aem-frontend/src/image-carousel/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import ImageViewer from './image-viewer';
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/scss/alice-carousel.scss";
 
 class ImageCarousel extends React.Component {
     constructor() {
@@ -8,25 +10,46 @@ class ImageCarousel extends React.Component {
 
         this.state = {
             activeIndex: 0,
+            mouseDragEnabled: true,
+            swipeDisabled: false,
             thumbnailStyles: {}
         };
     }
 
+    handleImageViewerCalculate = data => this.setState({thumbnailStyles: { width: `${data.imageWidth}px` }});
+    handleZoomIn = () => this.setState({ mouseDragEnabled: false, swipeDisabled: true });
+    handleZoomOut = () => this.setState({ mouseDragEnabled: true, swipeDisabled: false });
+    handleSlideChanged = e => this.setState({ activeIndex: e.slide });
+
     render() {
         return <div className="image-carousel">
             <div className="image-viewer-wrapper">
-                <ImageViewer
-                    template={this.props.templates[0]}
-                    widths={this.props.widths}
-                    onCalculate={this.handleImageViewerCalculate} />
+                <AliceCarousel
+                    items={this.getImageViewerComponents()}
+                    buttonsDisabled={true}
+                    dotsDisabled={true}
+                    mouseDragEnabled={this.state.mouseDragEnabled}
+                    swipeDisabled={this.state.swipeDisabled}
+                    slideToIndex={this.state.activeIndex}
+                    onSlideChanged={this.handleSlideChanged} />
             </div>
             <div className="image-thumbnail-wrapper" style={this.state.thumbnailStyles}>
-                thumbnails
+                {this.getThumbnails()}
             </div>
         </div>
     }
 
-    handleImageViewerCalculate = data => this.setState({thumbnailStyles: { width: `${data.imageWidth}px` }});
+    getImageViewerComponents = () => this.props.templates.map(template => this.mapTemplateToImageViewer(template));
+
+    mapTemplateToImageViewer = template => <ImageViewer
+        key={template}
+        template={template}
+        widths={this.props.widths}
+        onZoomIn={this.handleZoomIn}
+        onZoomOut={this.handleZoomOut}
+        onCalculate={this.handleImageViewerCalculate} />;
+
+    getThumbnails = () => <></>;
 }
 
 ImageCarousel.propTypes = {
