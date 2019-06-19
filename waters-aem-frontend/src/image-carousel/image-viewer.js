9269ed834815851502110fa597901a71320edd29
Garcia, Josue <josue.garcia@icfnext.com>
2019-06-19 11:59:11 -0500

    BTWCT-839: Image Gallery Component  => CSS browser hacks for zoom icon

--- waters-aem-frontend/src/image-carousel/image-viewer.js
+++ waters-aem-frontend/src/image-carousel/image-viewer.js
@@ -21,6 +21,9 @@ class ImageViewer extends React.Component {
     handleOnDragStart = e => e.preventDefault();
 
     handleMagnifyClick = () => {
+        // clear background position so subsequent clicks
+        // position the background to the center of the image
+        // which is specified in the stylesheet ruleset
         this.figureRef.current.style.backgroundPosition = "";
 
         const magnified = !this.state.magnified;
@@ -70,6 +73,9 @@ class ImageViewer extends React.Component {
     };
 
     handleFigureTouchStart = e => {
+        // touch-action property prevents scrolling during touchmove event
+        // check if browser supports this property so locking scrolling is
+        // handled using a stylesheet ruleset
         if (CSS && !CSS.supports("touch-action", "none")) {
             document.body.classList.add("lock-scroll");
         }
@@ -84,20 +90,7 @@ class ImageViewer extends React.Component {
     render() {
         return <div ref={this.containerRef} className="image-viewer-container">
         <div className="image-viewer-container__image-display">
-          <figure
-            onDragStart={this.handleOnDragStart}
-            ref={this.figureRef}
-            className={`image-viewer-container__image-figure image-viewer-container__image-figure--${this.state.magnified}`}
-            style={this.getFigureStyle()}
-            onMouseMove={this.handleFigureMouseMove}
-            onTouchStart={this.handleFigureTouchStart}
-            onTouchEnd={this.handleFigureTouchEnd}
-            onTouchMove={this.handleFigureTouchMove}>
-            <img
-            className="image-viewer-container__image-element"
-            src={this.state.imageSrc}
-            alt=""/>
-          </figure>
+          {this.renderImageDisplay()}
           <div className="image-viewer-container__image-zoom">
             <div>{this.renderZoomIcon()}</div>
           </div>
@@ -105,12 +98,24 @@ class ImageViewer extends React.Component {
       </div>
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        // check if image rendition as changed
+        if (prevState.imageSrc !== this.state.imageSrc) {
+            // delay so the figure element rendering completes
+            // the figure element is referenced to get image height
+            // 1 second delay works on all browser and devices
+            setTimeout(() => this.setStateImageHeight(), 1000);
+        }
+
+        if (prevState.imageWidth !== this.state.imageWidth && this.props.onCalculate) {
+            this.props.onCalculate(this.state);
+        }
+    }
+
     componentDidMount() {
         this.calculateWidth();
 
-        // delay so the figure element contains the calculated image
-        setTimeout(() => this.setStateImageHeight(), 500);
-
+        // this is for desktop
         window.addEventListener("resize", this.calculateWidth);
 
         // this is for iPad orientation
@@ -126,10 +131,27 @@ class ImageViewer extends React.Component {
         window.removeEventListener("deviceorientation", this.calculateWidth);
     }
 
+    renderImageDisplay = () => <figure
+        ref={this.figureRef}
+        className={`image-viewer-container__image-figure image-viewer-container__image-figure--${this.state.magnified}`}
+        style={this.getFigureStyle()}
+        onDragStart={this.handleOnDragStart}
+        onMouseMove={this.handleFigureMouseMove}
+        onTouchStart={this.handleFigureTouchStart}
+        onTouchEnd={this.handleFigureTouchEnd}
+        onTouchMove={this.handleFigureTouchMove}>
+        
+        <img
+            className="image-viewer-container__image-element"
+            src={this.state.imageSrc}
+            alt={this.props.alt}/>
+    </figure>
+
     renderZoomIcon = () => this.state.magnified
         ? <ReactSVG src={this.props.zoomOutIcon} onClick={this.handleMagnifyClick} /> 
         : <ReactSVG src={this.props.zoomInIcon} onClick={this.handleMagnifyClick} />;
 
+    // get the largest width that is less than the container width
     getClosestWidth = containerWidth => this.props.widths.reduce((prev, curr) => curr > containerWidth ? prev : curr);
 
     calculateWidth = () => {
@@ -137,21 +159,12 @@ class ImageViewer extends React.Component {
         const imageWidth = this.getClosestWidth(containerWidth);
         const imageSrc = this.props.template.replace(/{{width}}/gi, imageWidth);
 
-        this.setState({
-            containerWidth,
-            imageWidth,
-            imageSrc
-        });
-
-        if (this.props.onCalculate) {
-            setTimeout(() => {
-                this.setStateImageHeight();
-                this.props.onCalculate(this.state), 500
-            });
-        }
+        this.setState({containerWidth, imageWidth, imageSrc});
     };
 
     setStateImageHeight = () => {
+        // keep tracking of image height to calculate zoomin-in percentage
+        // which requires increasing background size (width and height)
         const imageHeight = this.figureRef.current.getBoundingClientRect().height;
         this.setState({imageHeight});
     }
@@ -160,17 +173,15 @@ class ImageViewer extends React.Component {
         this.state.magnified
         ? {
             backgroundImage: `url(${this.state.imageSrc})`,
-            backgroundSize: `${this.state.imageWidth * 2}px ${this.state.imageHeight * 2}px`,
-            maxWidth: `${this.state.imageWidth}px`
+            backgroundSize: `${this.state.imageWidth * 2}px ${this.state.imageHeight * 2}px`
         }
-        : {
-            maxWidth: `${this.state.imageWidth}px`
-        };
+        : {};
 }
 
 ImageViewer.propTypes = {
     template: PropTypes.string.isRequired,
     widths: PropTypes.arrayOf(PropTypes.string).isRequired,
+    alt: PropTypes.string,
     zoomInIcon: PropTypes.string.isRequired,
     zoomOutIcon: PropTypes.string.isRequired,
     onCalculate: PropTypes.func,
