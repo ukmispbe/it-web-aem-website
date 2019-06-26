6546ff08230a3391168465ba9b6d20897e09e02f
LaPaglia <nick.lapaglia@icfolson.com>
2019-06-26 15:09:28 -0500

    Removed extends from Commerce class

--- waters-aem-core/src/main/java/com/waters/aem/core/components/structure/page/Commerce.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/structure/page/Commerce.java
@@ -3,7 +3,6 @@ package com.waters.aem.core.components.structure.page;
 import com.citytechinc.cq.component.annotations.Component;
 import com.citytechinc.cq.component.annotations.DialogField;
 import com.citytechinc.cq.component.annotations.widgets.TextField;
-import com.icfolson.aem.library.core.components.AbstractComponent;
 import com.icfolson.aem.library.core.constants.ComponentConstants;
 import com.waters.aem.core.constants.WatersConstants;
 import org.apache.sling.api.resource.Resource;
@@ -20,7 +19,7 @@ import javax.inject.Inject;
     fileName = Commerce.FILE_NAME,
     touchFileName = Commerce.FILE_NAME)
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public class Commerce extends AbstractComponent {
+public class Commerce {
 
     static final String FILE_NAME = "commerce";
 
