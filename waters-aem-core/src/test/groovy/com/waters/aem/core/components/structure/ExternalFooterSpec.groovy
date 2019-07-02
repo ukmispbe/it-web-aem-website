0c918d2dd3030be985d13e29025e996976897a04
LaPaglia <nick.lapaglia@icfolson.com>
2019-07-02 10:49:55 -0500

    Updated external footer unit tests

--- waters-aem-core/src/test/groovy/com/waters/aem/core/components/structure/ExternalFooterSpec.groovy
+++ waters-aem-core/src/test/groovy/com/waters/aem/core/components/structure/ExternalFooterSpec.groovy
@@ -1,6 +1,8 @@
 package com.waters.aem.core.components.structure
 
 import com.waters.aem.core.WatersSpec
+import com.waters.aem.core.components.content.links.BasicLink
+import com.waters.aem.core.components.content.links.IconOnlyLink
 import spock.lang.Unroll
 
 @Unroll
@@ -14,12 +16,28 @@ class ExternalFooterSpec extends WatersSpec {
                         externalfooter(
                             logoLink: "/content/waters/page1.html",
                             logoAltText: "Alternative text",
-                            copyrightText: "Waters 2019"
-                        )
+                            copyrightText: "Waters 2019",
+                            cookiesLink: "https://www.waters.com/134982469"
+
+                        ) {
+                            logoImage(fileReference: "/content/dam/waters/logo.png")
+
+                            footerLinks {
+                                item1(link: "www.waters.com", text: "waters", external: true)
+                                item2(link: "www.ta.com", text: "ta", external: false)
+                            }
+
+                            socialLinks {
+                                item1(link: "https://www.facebook.com", linkIcon: "/content/dam/waters/brand-assets/icons/facebook.svg")
+                                item2(link: "https://twitter.com", linkIcon: "/content/dam/waters/brand-assets/icons/twitter.svg")
+                            }
+                        }
                     }
                 }
             }
         }
+        slingContext.addModelsForClasses(BasicLink)
+        slingContext.addModelsForClasses(IconOnlyLink)
     }
 
     def "get logo link"() {
@@ -51,4 +69,58 @@ class ExternalFooterSpec extends WatersSpec {
         expect:
         externalFooter.copyrightText == "Waters 2019"
     }
+
+    def "get cookies link"() {
+        setup:
+        def externalFooter = requestBuilder.build {
+            path = "/content/waters/one/jcr:content/externalfooter"
+        }.adaptTo(ExternalFooter)
+
+        expect:
+        externalFooter.cookiesLink.href == "https://www.waters.com/134982469"
+    }
+
+    def "get logo image"() {
+        setup:
+        def externalFooter = requestBuilder.build {
+            path = "/content/waters/one/jcr:content/externalfooter"
+        }.adaptTo(ExternalFooter)
+
+        expect:
+        externalFooter.logoImage.fileReference == "/content/dam/waters/logo.png"
+    }
+
+    def "get footer links"() {
+        setup:
+        def externalFooter = requestBuilder.build {
+            path = "/content/waters/one/jcr:content/externalfooter"
+        }.adaptTo(ExternalFooter)
+
+        expect:
+        externalFooter.footerLinks.size() == 2
+
+        and:
+        externalFooter.footerLinks.text == ["waters", "ta"]
+
+        and:
+        externalFooter.footerLinks.link.href == ["www.waters.com", "www.ta.com"]
+    }
+
+    def "get social links"() {
+        setup:
+        def externalFooter = requestBuilder.build {
+            path = "/content/waters/one/jcr:content/externalfooter"
+        }.adaptTo(ExternalFooter)
+
+        expect:
+        externalFooter.socialLinks.size() == 2
+
+        and:
+        externalFooter.socialLinks.link.href == ["https://www.facebook.com", "https://twitter.com"]
+
+        and:
+        externalFooter.socialLinks.linkIcon == ["/content/dam/waters/brand-assets/icons/facebook.svg", "/content/dam/waters/brand-assets/icons/twitter.svg"]
+    }
+
+
 }
