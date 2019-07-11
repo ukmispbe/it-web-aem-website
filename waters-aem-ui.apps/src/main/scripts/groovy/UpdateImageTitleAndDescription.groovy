407105a4d032f8e0f0d73626e0fba432a0a0905b
LaPaglia <nick.lapaglia@icfolson.com>
2019-07-11 09:50:21 -0500

    Added script to update Job Identifier property

--- waters-aem-ui.apps/src/main/scripts/groovy/UpdateImageTitleAndDescription.groovy
+++ waters-aem-ui.apps/src/main/scripts/groovy/UpdateImageTitleAndDescription.groovy
@@ -1,33 +1,33 @@
-final def page = getPage('/content/waters/language-masters/en/library/application-notes')
-final def query = buildQuery(page)
+final def rootPath = '/content/dam/waters/app-notes'
+final def query = buildQuery(rootPath)
 final def result = query.execute()
 
-def buildQuery(page){
+def buildQuery(rootPath){
     def queryManager = session.workspace.queryManager
-    def queryString = "SELECT * FROM [nt:base] WHERE ISDESCENDANTNODE([$page.path]) and ([sling:resourceType] = 'waters/components/structure/page' or [sling:resourceType] = 'waters/components/content/image')"
+    def queryString = "SELECT * FROM [dam:Asset] WHERE ISDESCENDANTNODE([$rootPath])"
     queryManager.createQuery(queryString, "JCR-SQL2")
 }
 
 result.nodes.each { node ->
-    node.getProperty('sling:resourceType').getString() == 'waters/components/structure/page' ?  processThumbnail(node) : processImage(node)
 
-    session.save()
-}
+    if(node.getName().endsWith('jpg')) {
 
+        def name = node.getName()
 
-def processThumbnail(node) {
-    if (node.hasNode('thumbnailImage')){
+        def metadataNode = getMetadataNode(node)
 
-        def thumbnailNode = node.getNode('thumbnailImage')
-
-        if(thumbnailNode.hasProperty('fileReference') && thumbnailNode.getProperty('fileReference').getString().endsWith('png')) {
-            thumbnailNode.setProperty('fileReference', thumbnailNode.getProperty('fileReference').getString().replaceAll('png', 'jpg'))
+        if(!metadataNode.hasProperty('photoshop:TransmissionReference')) {
+            metadataNode.setProperty('photoshop:TransmissionReference', name.substring(0, name.indexOf('-')))
         }
+
+        session.save()
     }
+
 }
 
-def processImage(node) {
-    if (node.hasProperty('fileReference') && node.getProperty('fileReference').getString().endsWith('png')) {
-        node.setProperty('fileReference', node.getProperty('fileReference').getString().replaceAll('png', 'jpg'))
-    }
+def getMetadataNode(node) {
+    def contentNode = node.getNode('jcr:content')
+
+    return contentNode.getNode('metadata')
 }
+
