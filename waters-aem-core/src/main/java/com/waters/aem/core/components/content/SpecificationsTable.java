ddf2ad555ed074489144441cd960d057b391aa56
LaPaglia <nick.lapaglia@icfolson.com>
2019-07-13 06:38:07 -0500

    [BTWCT-967] Initial development

---
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/SpecificationsTable.java
@@ -0,0 +1,51 @@
+package com.waters.aem.core.components.content;
+
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ExporterConstants;
+import com.citytechinc.cq.component.annotations.Component;
+import com.waters.aem.core.commerce.models.Classification;
+import com.waters.aem.core.commerce.models.Sku;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import java.util.List;
+
+@Component(value = "Specifications Table")
+@Model(adaptables = SlingHttpServletRequest.class,
+    adapters = { SpecificationsTable.class, ComponentExporter.class },
+    resourceType = SpecificationsTable.RESOURCE_TYPE,
+    defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME,
+    extensions = ExporterConstants.SLING_MODEL_EXTENSION)
+public final class SpecificationsTable implements ComponentExporter {
+
+    public static final String RESOURCE_TYPE = "waters/components/content/specificationstable";
+
+    private Logger log = LoggerFactory.getLogger(SpecificationsTable.class);
+
+    @Inject
+    private Sku sku;
+
+    public Sku getSku() {
+        sku.getClassifications().forEach(item-> log.info("item ---- {}", item.getTitle()));
+        log.info("SkU Test Classifications - {}", sku.getPath());
+        return sku;
+    }
+
+    public List<Classification> getSpecifications() {
+        return sku.getClassifications();
+    }
+
+    @Nonnull
+    @Override
+    public String getExportedType() {
+        return RESOURCE_TYPE;
+    }
+}
