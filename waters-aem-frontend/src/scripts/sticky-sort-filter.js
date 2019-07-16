5adceac5e0d5db2b4a2b2742d7aaebd806ef427c
Garcia, Josue <josue.garcia@icfnext.com>
2019-07-16 08:13:26 -0500

    BTWCT-876: SKU details component - Sku Details Component should be Sticky => Make the header un-sticky on all breakpoints, adjust search modal to new header height

--- waters-aem-frontend/src/scripts/sticky-sort-filter.js
+++ waters-aem-frontend/src/scripts/sticky-sort-filter.js
@@ -1,23 +1,19 @@
 import screenSizes from './screenSizes';
 import Sticky from './sticky';
-import domElements from './domElements';
 
 const SortFilterSticky = () => {
     // make sort filter sticky on mobile only
     if (screenSizes.isMobile()) {
-        const headerElement = domElements.getHeader();
-        const topTreshold = headerElement ? headerElement.getBoundingClientRect().bottom : 0;
-        const topSticky = topTreshold === 0 ? 0 : topTreshold - 1;
-        const stickyFunc = Sticky('btn-show-sort-filter', 'btn-show-sort-filter--sticky', topTreshold, topSticky);
+        const stickyFunc = Sticky('btn-show-sort-filter', 'btn-show-sort-filter--sticky', 0, 0);
 
         if (stickyFunc) {
             // target element was found so proceed with binding the event listener
             const sortFilterStickyEventListener = function() {
                 // need to recheck screen size in case user rotates device
-                if (!screenSizes.isMobile()) { return }
-
-                // execute the sticky function
-                stickyFunc();
+                if (screenSizes.isMobile()) { 
+                    // execute the sticky function
+                    stickyFunc();
+                 }
             }
 
             window.addEventListener('scroll', sortFilterStickyEventListener);
