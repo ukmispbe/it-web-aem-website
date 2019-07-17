12c4f19cd6d429d5e4baa58be82de96cc80f5217
Garcia, Josue <josue.garcia@icfnext.com>
2019-07-17 17:35:41 -0500

    BTWCT-991: Image Gallery: Mobile/Tablet: Fade is not visible when thumbnails are more => added left and right gradients

--- waters-aem-frontend/src/scripts/screenSizes.js
+++ waters-aem-frontend/src/scripts/screenSizes.js
@@ -1,5 +1,6 @@
 const screenSizes = {
-    isMobile: () => window.matchMedia('(max-width: 650px)').matches
+    isMobile: () => window.matchMedia('(max-width: 650px)').matches,
+    isTabletAndUnder: () => window.matchMedia('(max-width: 1200px)').matches
 }
 
 export default screenSizes;
