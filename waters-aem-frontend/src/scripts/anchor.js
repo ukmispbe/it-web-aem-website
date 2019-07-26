fc10939e99988ffb8034d44e32c5069ede5dcd73
ukmispbe <Paul_Beard@waters.com>
2019-07-26 11:39:10 +0100

    BTWCT-1026 Anchor on Tablet Unable to scroll left or right

--- waters-aem-frontend/src/scripts/anchor.js
+++ waters-aem-frontend/src/scripts/anchor.js
@@ -77,11 +77,13 @@ var anchorSticky = (function() {
             this.getInViewElement();
         },
         setFixed: function() {
+            anchorChange();
             this.element.parentNode.style.height =
                 this.element.clientHeight + 'px';
-            this.element.classList.add(CSS_CLASS_ACTIVE);
+            this.element.classList.add(CSS_CLASS_ACTIVE);            
         },
         setRelative: function() {
+            anchorChange();
             this.element.classList.remove(CSS_CLASS_ACTIVE);
             this.element.parentNode.style.height = 'auto';
         },
@@ -157,13 +159,6 @@ function hideScrollBars(el) {
 }
 
 function anchorChange(el) {
-    var anchorList = document.querySelector('.cmp-anchor__list');
-    const isMobile = screenSizes.isMobile();
-    const isTabletAndUnder = screenSizes.isTabletAndUnder();
-    if (isTabletAndUnder && !isMobile) {
-        showScrollBars(anchorList);
-    }  
-
     clearGradients();
     var anchorElementId = document.getElementById('cmp-anchor');
     if (anchorElementId.scrollLeft > 0) {
@@ -211,9 +206,7 @@ var lhsGradientFade = document.getElementsByClassName(
 
 if (anchorList) {
     const isMobile = screenSizes.isMobile();
-
     const isTabletAndUnder = screenSizes.isTabletAndUnder();
-    
     const sectionContainers = document.querySelectorAll(
         '.cmp-section-container--collapse'
     );
@@ -241,9 +234,11 @@ if (anchorList) {
         anchorList.addEventListener('scroll', () => scrollWindow(anchorList));
         window.addEventListener('load', () => resizeWindow(anchorList));
         window.addEventListener('resize', () => resizeWindow(anchorList));
+
         if (isTabletAndUnder && !isMobile) {
             window.addEventListener('load', () => showScrollBars(anchorList));
         }   
+        
         var mediaQueryListener = window.matchMedia('(max-width: 650px)');
 
         function anchorChangeToMobile() {
