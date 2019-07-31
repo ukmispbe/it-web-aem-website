f439e2cffe8862ad4b7f29c3147689170cab6fea
Lakshmi <Lakshmi_valluripalli@waters.com>
2019-07-30 20:46:27 -0400

    Updated Link Utils to consider Waters.com legacy links as internal.

--- waters-aem-core/src/main/java/com/waters/aem/core/utils/LinkUtils.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/utils/LinkUtils.java
@@ -2,11 +2,12 @@ package com.waters.aem.core.utils;
 
 import com.icfolson.aem.library.api.link.Link;
 
-
 public final class LinkUtils {
 
+    private static final String WATERS_DOMAIN = "www.waters.com";
+
     public static Boolean isExternal(Link link) {
-        return link != null && !link.getHref().startsWith("/");
+        return link != null && !link.getHref().contains(WATERS_DOMAIN) && !link.getHref().startsWith("/");
     }
 
     public static Boolean isSvg(String icon) {
