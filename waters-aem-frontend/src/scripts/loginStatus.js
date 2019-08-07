2a8bfd4f4b8cbdc947ff03582b26e11807926964
TylerTessmann-ICFOlson <tyler.tessmann@icf.com>
2019-08-07 15:34:30 -0500

    [bugfix-0] - changed header text to use il18n expression for future lang translation, login status prefix now comes from data-attribute for same reason

--- waters-aem-frontend/src/scripts/loginStatus.js
+++ waters-aem-frontend/src/scripts/loginStatus.js
@@ -2,7 +2,6 @@ import domElements from '../scripts/domElements';
 
 const loggedInCookie = 'WatersLoginCookie';
 const greetingCookie = 'WatersGreetingCookie';
-const greetingPreFix = 'Hello, ';
 
 const loginStatus = {
     
@@ -12,6 +11,8 @@ const loginStatus = {
     getGreeting: () => { 
         let greeting = domElements.getCookie(greetingCookie);
         let loggedIn = domElements.getCookie(loggedInCookie) ? true : false;
+        let greetingPreFix = document.querySelector('.cmp-header__top-bar__nav .top-bar__nav__user__link .greeting-text').dataset.greeting || '';  
+            
         if (greeting && loggedIn) {
             greeting = greeting.replace(/["]+/g, '');
             return greetingPreFix + decodeURIComponent(greeting).trim();
