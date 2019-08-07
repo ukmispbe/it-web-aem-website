461f8787b422088f06180cae16467618a8455208
Chris Hogan <chris@hogan.io>
2019-08-07 08:59:23 -0600

    BTWCT-964 Adding Nick's template/backend change here adding locale, making a utility function which shows the date available in locale format

--- waters-aem-frontend/src/sku-details/utils/utils.js
+++ waters-aem-frontend/src/sku-details/utils/utils.js
@@ -1,10 +1,18 @@
 // Stub our for shared utility functions
 
 function dateFormatter(inputdate) {
-    // TODO: 
-    // Eventually, we will get a ticket to set date format to local settings based on country, or let user pick date format
-    // Example right now: availability date in stock app. Date comes from API as yyyy-mm-dd
-    return inputdate;
+    // TODO: Get locale from template when we are using the AEM templates. This data comes from the data-locale attribute on cmp-sku-details
+    // const searchAppContainer = document.getElementById('cmp-sku-details'); // TODO: move this up somewhere better if we need to use locale in sku anywhere else
+    const userLocale = 'en-US'
+    if(inputdate){
+        let splitDate = inputdate.split('-')
+        let constructedDate = new Date(Date.UTC(splitDate[0], splitDate[1], splitDate[2]));
+        return constructedDate.toLocaleDateString(userLocale, { year: 'numeric', month: 'long', day: 'numeric' });
+    }
+    else {
+        return '-';
+    }
+
 }
 
 export default {
