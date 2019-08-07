311b2681d0d16726ae10690da4afaa17f1d6e4b3
Eric Van Geem <eric.vangeem@icfnext.com>
2019-08-07 15:45:22 -0400

    [BTWCT-921] fix API endpoints.

--- waters-aem-core/src/main/java/com/waters/aem/core/services/commerce/WatersCommerceServiceConfiguration.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/services/commerce/WatersCommerceServiceConfiguration.java
@@ -7,10 +7,10 @@ import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 public @interface WatersCommerceServiceConfiguration {
 
     @AttributeDefinition(name = "Sku Availability Endpoint", description = "URL for the Sku Availability endpoint")
-    String skuAvailabilityUrl() default "https://dev-www.waters.com:8443/product/v1/availability/{partnumber}/{isocode}";
+    String skuAvailabilityUrl() default "https://dev-www.waters.com:8443/api/waters/product/v1/availability/{partnumber}/{isocode}";
 
     @AttributeDefinition(name = "Customer Price Endpoint", description = "URL for the Sku Customer Price endpoint")
-    String skuCustomerPriceUrl() default "https://dev-www.waters.com:8443/product/v1/customerprice";
+    String skuCustomerPriceUrl() default "https://dev-www.waters.com:8443/api/waters/product/v1/customerprice";
 
     @AttributeDefinition(name = "Cart Endpoint", description = "URL for the Cart endpoint")
     String cartUrl() default "https://www.waters.com/waters/ajax.htm?handler=shoppingHandler&action=processExternalCart";
