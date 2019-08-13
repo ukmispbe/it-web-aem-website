66c8b524264a4f36b0403f655a6dc8e670dbe5af
TylerTessmann-ICFOlson <tyler.tessmann@icf.com>
2019-08-13 10:57:55 -0500

    [BTWCT-1029] - ios 10 alignment bugs, L1 overlay on tablet fixed, padding issue on L1 desktop

--- waters-aem-frontend/src/scripts/screenSizes.js
+++ waters-aem-frontend/src/scripts/screenSizes.js
@@ -1,7 +1,7 @@
 const screenSizes = {
     isMobile: () => window.matchMedia('(max-width: 650px)').matches,
     isTabletAndUnder: () => window.matchMedia('(max-width: 1200px)').matches,
-    isTabletAndOver: () => window.matchMedia('(min-width: 1201px)').matches
+    isTabletAndOver: () => window.matchMedia('(min-width: 651px)').matches
 }
 
 export default screenSizes;
