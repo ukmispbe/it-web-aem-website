bee44f586ba61d5b044da8b3009d1b36123b9eae
TylerTessmann-ICFOlson <tyler.tessmann@icf.com>
2019-08-16 10:49:19 -0500

    BTWCT-1029 - bug testing

--- waters-aem-frontend/src/scripts/navigation-overlay.js
+++ waters-aem-frontend/src/scripts/navigation-overlay.js
@@ -9,36 +9,43 @@ if (elem) {
 
    elem.parentElement.after(overlay);
 
+   const closeOverlay = () => {
+      console.log('close overlay');
+      if (screenSizes.isTabletAndOver()) {
+         console.log('right size');
+         overlay.style.opacity = "0";
+         overlay.style.visibility = "hidden";
+         overlay.style.transitionDelay = "0s, 0s";
+         elem.classList.remove('cmp-navigation--shadow');
+      }
+   };
+
+   const openOverlay = () => {
+      if (screenSizes.isTabletAndOver()) {
+         overlay.style.opacity = "0.5";
+         overlay.style.visibility = "visible";
+         overlay.style.transitionDelay = "0s, 0s";
+         elem.classList.add('cmp-navigation--shadow');
+      }
+   };
+
    Array.from(document.querySelector('.navigation .cmp-navigation .cmp-navigation__group').children).forEach(function(e){
       const level2Links = e.querySelector('.cmp-navigation__group');
 
       const level2LinksCount = (level2Links) ? level2Links.children.length : 0;
 
       if (level2LinksCount !== 0) {
-         e.addEventListener('mouseover', function() {
-            if (screenSizes.isTabletAndOver()) { 
-               overlay.style.opacity = "0.5";
-               overlay.style.visibility = "visible";
-               overlay.style.transitionDelay = "0s, 0s";
-               elem.classList.add('cmp-navigation--shadow');
-            }
-         });
-         
-         e.addEventListener('mouseleave', function() {
-            if (screenSizes.isTabletAndOver()) { 
-               overlay.style.opacity = "0";
-               overlay.style.visibility = "hidden";
-               overlay.style.transitionDelay = "0s, 0s";
-               elem.classList.remove('cmp-navigation--shadow');
-            }
-         });
+         e.addEventListener('mouseover', openOverlay);
+         e.addEventListener('mouseleave', closeOverlay);     
       }
-
+      
       const level1Link = e.querySelector('.cmp-navigation__container .cmp-navigation__item-link');
 
       if (level1Link) {
          level1Link.addEventListener('click', event => event.preventDefault());
       }
    });
-
+  
+   overlay.addEventListener('click', closeOverlay); 
+   document.body.addEventListener('click', event => { console.log('body click overlay', event.target, event.target.current)})
 }
