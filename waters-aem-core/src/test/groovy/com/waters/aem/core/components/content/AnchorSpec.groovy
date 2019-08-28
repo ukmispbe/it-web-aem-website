729c634964411fb0bac86e542344fbf78bd1b6cf
Eric Van Geem <eric.vangeem@icfnext.com>
2019-08-27 21:51:01 -0500

    add sku tests for anchor.

--- waters-aem-core/src/test/groovy/com/waters/aem/core/components/content/AnchorSpec.groovy
+++ waters-aem-core/src/test/groovy/com/waters/aem/core/components/content/AnchorSpec.groovy
@@ -2,6 +2,7 @@ package com.waters.aem.core.components.content
 
 import com.waters.aem.core.WatersSpec
 import com.waters.aem.core.components.content.applicationnotes.SectionContainer
+import com.waters.aem.core.constants.WatersConstants
 
 class AnchorSpec extends WatersSpec {
 
@@ -17,6 +18,34 @@ class AnchorSpec extends WatersSpec {
                             section2("sling:resourceType": SectionContainer.RESOURCE_TYPE, title: "Section 2")
                             image("sling:resourceType": Image.RESOURCE_TYPE)
                             section3("sling:resourceType": SectionContainer.RESOURCE_TYPE, title: "Section 3")
+                        }
+                    }
+                }
+
+                products() {
+                    "186007517"() {
+                        "jcr:content"(
+                                "cq:template": WatersConstants.TEMPLATE_SKU_PAGE,
+                                "hybris:code": "186007517",
+                                "hybris:productResourcePath": "/etc/commerce/products/186/186007517") {
+                            anchor()
+                            section1("sling:resourceType": SectionContainer.RESOURCE_TYPE,
+                                    title: "Product Description") {
+                                par {
+                                    text("sling:resourceType": Text.RESOURCE_TYPE)
+                                }
+                            }
+                            section2("sling:resourceType": SectionContainer.RESOURCE_TYPE, title: "Specifications") {
+                                par {
+                                    specificationstable("sling:resourceType": SpecificationsTable.RESOURCE_TYPE)
+                                }
+                            }
+                            section3("sling:resourceType": SectionContainer.RESOURCE_TYPE, title: "Related Products") {
+                                par {
+                                    skulist("sling:resourceType": SkuList.RESOURCE_TYPE)
+                                }
+                            }
+                            section4("sling:resourceType": SectionContainer.RESOURCE_TYPE, title: "Product Support")
                         }
                     }
                 }
@@ -38,5 +67,15 @@ class AnchorSpec extends WatersSpec {
 
         and:
         anchor.links*.title == ["Section 1", "Section 2", "Section 3"]
+    }
+
+    def "get anchor link count with empty related products section"() {
+        setup:
+        def anchor = requestBuilder.build {
+            path = "/content/waters/products/186007517/jcr:content/anchor"
+        }.adaptTo(Anchor)
+
+        expect:
+        anchor.links.size() == 3
     }
 }
