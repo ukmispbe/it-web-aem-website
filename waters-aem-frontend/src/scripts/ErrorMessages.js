3171e03dde5f468f456396f1896db8be39e21ff5
ukmispbe <Paul_Beard@waters.com>
2019-09-06 10:41:40 +0100

    BTWCT-1137-generic-error-handling-from-service New Error Messages File

---
+++ waters-aem-frontend/src/scripts/ErrorMessages.js
@@ -0,0 +1,37 @@
+let messageObj = {};
+
+function ErrorMessages (errorObj) {
+
+    const skuDetailsConfig = JSON.parse(
+        document.getElementById('commerce-configs-json').innerHTML
+    );
+
+    switch (errorObj.status) {
+        case 500: 
+            messageObj = {
+                serviceUnavailable:     skuDetailsConfig.errorInfo.serviceUnavailable,
+                tryAgainLater:          skuDetailsConfig.errorInfo.tryAgainLater,
+                img:                    "lowStockIcon",
+                anErrorHasOccurred:     skuDetailsConfig.errorInfo.anErrorHasOccurred,
+                wereSorry:              skuDetailsConfig.errorInfo.wereSorry
+            }
+            break;      
+        default:  
+            messageObj = {
+                serviceUnavailable:   skuDetailsConfig.errorInfo.serviceUnavailable,
+                tryAgainLater:      skuDetailsConfig.errorInfo.tryAgainLater,
+                img:                "outOfStockIcon",
+                anErrorHasOccurred: skuDetailsConfig.errorInfo.anErrorHasOccurred,
+                wereSorry:          skuDetailsConfig.errorInfo.wereSorry
+            }
+            break;
+    }
+    return messageObj;
+}
+
+export default {
+    ErrorMessages: ErrorMessages
+}
+    
+	
+	
