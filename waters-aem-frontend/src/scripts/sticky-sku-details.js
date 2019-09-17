d5643929210fd081b42948f04db05c632dba9976
TylerTessmann-ICFOlson <tyler.tessmann@icf.com>
2019-09-17 12:45:56 -0500

    [BTWCT-1167] - regional commerce rules for sku details sticky (hide mobile sku details sticky for partial commerce while logged out)

--- waters-aem-frontend/src/scripts/sticky-sku-details.js
+++ waters-aem-frontend/src/scripts/sticky-sku-details.js
@@ -1,17 +1,47 @@
 import sticky from './stickyService';
+import LoginStatus from '../scripts/loginStatus';
+import ScreenSizes from '../scripts/screenSizes';
 
 const SKUDetatilsSticky = () => {
-    sticky.add({
-        element: document.querySelector('.cmp-sku-details'),
-        priority: 1,
-        modifier: 'cmp-sku-details--sticky',
-        offset: {
-            position: 'bottom',
-            amount: 60,
-        },
-        fillHeight: 50,
-        stickyHeight: 92,  // setting to 92px to remove gap between anchor and sku-details sticky
-    });
+    const skuDetails = document.querySelector('.cmp-sku-details');
+    if (skuDetails) { 
+        const commerceState = skuDetails.dataset.commerce;
+        if (commerceState != 'DISABLED') { 
+            sticky.add({
+                element: skuDetails,
+                priority: 1,
+                modifier: 'cmp-sku-details--sticky',
+                offset: {
+                    position: 'bottom',
+                    amount: 60,
+                },
+                conditions: (element) => { 
+                    const commerceState = element.dataset.commerce;
+                    const loggedIn = LoginStatus.state();
+                    const isMobile = ScreenSizes.isMobile();
+
+                    if (commerceState == 'PARTIAL_ENABLED') {
+                        if (!loggedIn) {
+                            if (isMobile) {
+                                return false;
+                            } else { 
+                                return true;
+                            }
+                        } else { 
+                            return true;
+                        }
+                    } else { 
+                        return true;
+                    }
+                },
+                fillHeight: 50,
+                stickyHeight: 92,  // setting to 92px to remove gap between anchor and sku-details sticky
+            });
+
+
+        }
+    }   
 };
+
 
 document.addEventListener('DOMContentLoaded', SKUDetatilsSticky);
