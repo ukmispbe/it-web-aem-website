4dfb63c82bcf2b3db0db1918a0954b155b1dfb14
Cole Calamos <cole.calamos@icfnext.com>
2019-10-01 15:15:23 -0500

    BTWCT-1542: Added error checking for non-search pages

--- waters-aem-frontend/src/scripts/mobile-search-scroll.js
+++ waters-aem-frontend/src/scripts/mobile-search-scroll.js
@@ -1,23 +1,30 @@
 import screenSizes from './screenSizes';
 
 const androidSuggestionFix = () => {
-    if (screenSizes.isMobile()) {
+    if (screenSizes.isMobile() && document.getElementsByClassName('cmp-search__sort-filter__container').length) {
         let windowHeight = document.documentElement.clientHeight;
 
         window.addEventListener('resize', () => {
             let newHeight = document.documentElement.clientHeight;
 
             let sortContainer = document.getElementsByClassName('cmp-search__sort-filter__container')[0];
-            let openedFacet = sortContainer.getElementsByClassName('cmp-search-filters__filter expanded')[0];
-            let searchField = openedFacet.getElementsByClassName('cmp-search-filters__filter__search')[0];
+            if (sortContainer) {
+                let openedFacet = sortContainer.getElementsByClassName('cmp-search-filters__filter expanded')[0];
 
-            if (windowHeight > newHeight) {
-                sortContainer.scrollTo(0, (
-                    sortContainer.scrollTop +
-                    (searchField.getBoundingClientRect().top - sortContainer.getBoundingClientRect().top)
-                ));
-            } else if (newHeight > windowHeight) {
-                windowHeight = newHeight;
+                if (openedFacet) {
+                    let searchField = openedFacet.getElementsByClassName('cmp-search-filters__filter__search')[0];
+
+                    if (windowHeight > newHeight) {
+                        if (searchField) {
+                            sortContainer.scrollTo(0, (
+                                sortContainer.scrollTop +
+                                (searchField.getBoundingClientRect().top - sortContainer.getBoundingClientRect().top)
+                            ));
+                        }
+                    } else if (newHeight > windowHeight) {
+                        windowHeight = newHeight;
+                    }
+                }
             }
         });
     }
