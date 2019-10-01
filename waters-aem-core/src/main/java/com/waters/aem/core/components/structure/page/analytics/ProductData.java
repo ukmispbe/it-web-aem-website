bbe2fd4ae6fb730a0b31622c72ea80e1043d3f1e
Eric Van Geem <eric.vangeem@icfnext.com>
2019-10-01 15:41:09 -0500

    [BTWCT-611] refactor to use DisplayableSku.

--- waters-aem-core/src/main/java/com/waters/aem/core/components/structure/page/analytics/ProductData.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/structure/page/analytics/ProductData.java
@@ -1,5 +1,6 @@
 package com.waters.aem.core.components.structure.page.analytics;
 
+import com.waters.aem.core.commerce.models.DisplayableSku;
 import com.waters.aem.core.commerce.models.Sku;
 import com.waters.aem.core.components.SiteContext;
 import org.apache.sling.api.resource.Resource;
@@ -8,8 +9,7 @@ import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 
 import javax.inject.Inject;
-import java.math.BigDecimal;
-import java.text.NumberFormat;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,34 +22,27 @@ public class ProductData {
     private Sku sku;
 
     @Self
-    SiteContext siteContext;
+    private SiteContext siteContext;
 
     public List<Map<String, Object>> getProducts() {
-        List<Map<String, Object>> productList = sku.getRelatedSkus().stream()
-        .map(relatedSku -> getProductProperties(relatedSku))
-        .collect(Collectors.toList());
+        final List<Map<String, Object>> productList = Arrays.asList(getProductProperties(sku));
 
-        productList.add(0, getProductProperties(sku));
+        productList.addAll(sku.getRelatedSkus().stream()
+                .map(this::getProductProperties)
+                .collect(Collectors.toList()));
 
-        return  productList;
+        return productList;
     }
 
-    public Map<String, Object> getProductProperties(final Sku sku) {
+    private Map<String, Object> getProductProperties(final Sku sku) {
         final Map<String, Object> properties = new HashMap<>();
+        final DisplayableSku displayableSku = new DisplayableSku(sku, siteContext);
 
         properties.put("name", sku.getTitle());
-        properties.put("price", getPrice());
+        properties.put("price", displayableSku.getFormattedPrice());
         properties.put("sku", sku.getCode());
 
         return properties;
 
     }
-
-    public String getPrice() {
-        final BigDecimal price = sku.getPrice(siteContext.getLocaleWithCountry().getCountry(), siteContext
-        .getCurrencyIsoCode());
-
-        return  price == null ? null : NumberFormat.getCurrencyInstance(siteContext.getLocaleWithCountry()).format(price);
-    }
-
 }
