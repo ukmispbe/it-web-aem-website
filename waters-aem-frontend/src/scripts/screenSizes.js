82e5d9c46166d3f0119d6e742d0ae48f00eb4b7e
Garcia, Josue <josue.garcia@icfnext.com>
2019-10-07 12:12:31 -0500

    BTWCT-1777: SKU Detail:Anchor is not pointing to respective section on click

--- waters-aem-frontend/src/scripts/screenSizes.js
+++ waters-aem-frontend/src/scripts/screenSizes.js
@@ -1,7 +1,8 @@
 const screenSizes = {
     isMobile: () => window.matchMedia('(max-width: 650px)').matches,
     isTabletAndUnder: () => window.matchMedia('(max-width: 1200px)').matches,
-    isTabletAndOver: () => window.matchMedia('(min-width: 651px)').matches
+    isTabletAndOver: () => window.matchMedia('(min-width: 651px)').matches,
+    isTablet: () => window.matchMedia('(min-width: 651px) and (max-width: 1200px)').matches
 }
 
 export default screenSizes;
