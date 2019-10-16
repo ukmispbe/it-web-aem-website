f36eb2bf62d20309f7fdff03591e940959a82202
LaPaglia <nick.lapaglia@icfolson.com>
2019-10-16 08:18:32 -0500

    [BTWCT-1405] Added check to experience fragments to display script and refactored based on review feedback

--- waters-aem-core/src/main/java/com/waters/aem/core/components/structure/page/ScriptHelper.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/structure/page/ScriptHelper.java
@@ -1,33 +1,30 @@
 package com.waters.aem.core.components.structure.page;
 
+import com.adobe.cq.xf.ExperienceFragmentsConstants;
+import com.icfolson.aem.library.api.node.ComponentNode;
 import com.icfolson.aem.library.api.page.PageDecorator;
 import com.icfolson.aem.library.core.node.predicates.ComponentNodeResourceTypePredicate;
 import com.waters.aem.core.components.SiteContext;
 import com.waters.aem.core.components.content.Video;
 import com.waters.aem.core.services.brightcove.BrightcoveService;
 import com.waters.aem.core.utils.BrightcoveUtils;
-import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.Self;
-import org.apache.sling.models.factory.ModelFactory;
 
 import javax.inject.Inject;
 import java.util.Collections;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-@Model(adaptables = { SlingHttpServletRequest.class, Resource.class }, defaultInjectionStrategy =
-    DefaultInjectionStrategy.OPTIONAL)
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class ScriptHelper {
 
     @OSGiService
     private BrightcoveService brightcoveService;
-
-    @OSGiService
-    private ModelFactory modelFactory;
 
     @Inject
     private PageDecorator currentPage;
@@ -35,22 +32,8 @@ public class ScriptHelper {
     @Self
     private SiteContext siteContext;
 
-    @Self
-    private SlingHttpServletRequest request;
-
     public boolean getPageHasVideoComponent() {
-        return hasVideoComponent();
-    }
-
-    private boolean hasVideoComponent() {
-        return !currentPage.getComponentNode()
-        .transform(contentNode -> contentNode.findDescendants(
-        new ComponentNodeResourceTypePredicate(Video.RESOURCE_TYPE))
-        .stream()
-        .map(componentNode -> modelFactory.getModelFromWrappedRequest(request, componentNode.getResource(),
-            Video.class))
-        .filter(Objects :: nonNull)
-        .collect(Collectors.toList())).or(Collections.emptyList()).isEmpty();
+        return hasVideoComponent(currentPage) || experienceFragmentHasVideoComponent();
     }
 
     public String getBrightcoveAccount() {
@@ -61,4 +44,31 @@ public class ScriptHelper {
         return BrightcoveUtils.getBrightcovePlayerId(siteContext, brightcoveService);
     }
 
+    private boolean hasVideoComponent(final PageDecorator pageDecorator) {
+        return !pageDecorator.getComponentNode().transform(contentNode -> contentNode.findDescendants(
+            new ComponentNodeResourceTypePredicate(Video.RESOURCE_TYPE))
+            .stream()
+            .filter(Objects :: nonNull)
+            .collect(Collectors.toList())).or(Collections.emptyList()).isEmpty();
+    }
+
+    private boolean experienceFragmentHasVideoComponent() {
+        return !currentPage.getComponentNode()
+            .transform(contentNode -> contentNode.findDescendants(
+            new ComponentNodeResourceTypePredicate(ExperienceFragmentsConstants.RT_EXPERIENCE_FRAGMENT_COMPONENT))
+            .stream()
+            .map(ComponentNode::getResource)
+            .filter(this::videoComponentExistsInFragment)
+            .filter(Objects :: nonNull)
+            .collect(Collectors.toList())).or(Collections.emptyList()).isEmpty();
+    }
+
+    private boolean videoComponentExistsInFragment(Resource resource) {
+        ValueMap map = resource.getValueMap();
+
+        PageDecorator pageDecorator = resource.getResourceResolver()
+            .getResource(map.get(ExperienceFragmentsConstants.PN_FRAGMENT_PATH, "")).adaptTo(PageDecorator.class);
+
+        return pageDecorator != null && hasVideoComponent(pageDecorator);
+    }
 }
--- waters-aem-core/src/main/java/com/waters/aem/core/utils/BrightcoveUtils.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/utils/BrightcoveUtils.java
@@ -5,13 +5,15 @@ import com.waters.aem.core.services.brightcove.BrightcoveService;
 
 public class BrightcoveUtils {
 
-    public static String getBrightcoveAccount(SiteContext siteContext, BrightcoveService brightcoveService) {
+    public static String getBrightcoveAccount(final SiteContext siteContext, final BrightcoveService
+        brightcoveService) {
         final String countryCode = siteContext.getLocaleWithCountry().getCountry();
         return countryCode.equalsIgnoreCase("cn") ? brightcoveService.getChinaBrightcoveAccount() :
             brightcoveService.getBrightcoveAccount();
     }
 
-    public static String getBrightcovePlayerId(SiteContext siteContext, BrightcoveService brightcoveService) {
+    public static String getBrightcovePlayerId(final SiteContext siteContext, final BrightcoveService
+        brightcoveService) {
         final String countryCode = siteContext.getLocaleWithCountry().getCountry();
         return  countryCode.equalsIgnoreCase("cn") ? brightcoveService.getChinaBrightcovePlayerId() :
             brightcoveService.getBrightcovePlayerId();
