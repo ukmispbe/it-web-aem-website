d27839a8d16442e9fd7fdf741a502371ea491c72
Cole Calamos <cole.calamos@icfnext.com>
2019-10-17 16:46:46 -0500

    BTWCT-1595: Icons and validations for password initial work

--- waters-aem-frontend/src/forms/fields/input.js
+++ waters-aem-frontend/src/forms/fields/input.js
@@ -1,21 +1,38 @@
 import React from "react";
 import { functions } from "./patterns";
+import ReactSVG from "react-svg";
 
 const Input = ({
     type,
     name,
     label,
+    hasMatch,
+    icons,
     register,
     description,
     required,
     fieldErr,
-    validation
+    errors,
+    setError,
+    clearError,
+    validation,
+    matchRef
 }) => {
     const setValidation = () => {
         const obj = {};
 
         if (validation && validation.validateFnName) {
-            obj.validate = functions[validation.validateFnName];
+            if (validation.validateFnName === "matching") {
+                obj.validate = value => {
+                    return functions[validation.validateFnName](value, document.getElementById(matchRef));
+                };
+            } else if (validation.validateFnName === "password") {
+                obj.validate = value => {
+                    return functions[validation.validateFnName](value, document.getElementById(name), setError, clearError);
+                };
+            } else {
+                obj.validate = functions[validation.validateFnName];
+            }
         }
 
         return obj;
@@ -57,9 +74,10 @@ const Input = ({
         return obj;
     };
 
-    const displayMsg = () => {
+    const displayMsg = (name) => {
         if (validation) {
             if (fieldErr) {
+                fieldErr.ref.classList.add('error');
                 if (fieldErr.type === "required") {
                     return fieldErr.message || validation.requiredMsg;
                 } else if (
@@ -89,21 +107,102 @@ const Input = ({
         return reg;
     };
 
+    const toggleEye = (e) => {
+        const parent = e.currentTarget.parentNode;
+        const onIcon = parent.querySelector('.showHide-icon');
+        const offIcon = parent.querySelector('.showHideOff-icon');
+
+        if (onIcon && offIcon) {
+            onIcon.classList.toggle('toggled');
+            offIcon.classList.toggle('toggled');
+
+            type = (onIcon.classList.contains('toggled')) ? "text" : "password";
+            parent.parentNode.querySelector('input').type = type;
+        }
+    };
+
+    const renderMatch = () => {
+        if (!hasMatch) {
+            return (<></>);
+        }
+
+        const newName = name.charAt(0).toUpperCase() + name.slice(1);
+        const confirmName = "confirm".concat(newName);
+        const confirmLabel = "Confirm ".concat(newName);
+        const newValidation = {
+            'validateFnName': 'matching',
+            "validationMsg": validation['nonMatchingMsg']
+        };
+
+        return (
+            <>
+                <Input
+                    type={type}
+                    name={confirmName}
+                    label={confirmLabel}
+                    hasMatch={false}
+                    icons={icons}
+                    register={register}
+                    description={description ? "Match for ".concat(newName) : ""}
+                    required={required}
+                    fieldErr={errors[confirmName]}
+                    errors={errors}
+                    setError={setError}
+                    validation={newValidation}
+                    matchRef={name}
+                />
+            </>
+        );
+    }
+
+    const renderIcons = () => {
+        if (!icons) {
+            return (<></>);
+        }
+
+        const eyeIcons = (
+            type === "password" ? (
+                <>
+                <ReactSVG src={icons.eyeIcon} className="showHide-icon" onClick={toggleEye} />
+                <ReactSVG src={icons.eyeOffIcon} className="showHideOff-icon toggled" onClick={toggleEye} />
+                </>
+            ) : (<></>)
+        );
+
+        return (<div className="cmp-form-field--icons">
+            {eyeIcons}
+            <ReactSVG src={icons.validIcon} className="valid-icon" />
+            <ReactSVG src={icons.invalidIcon} className="invalid-icon" />
+        </div>);
+    };
+
+    const renderInput = (name, label) => {
+        return (
+            <>
+                <label htmlFor={name}>{label}</label>
+                <div className="cmp-form-field--input">
+                    <input
+                        type={type}
+                        name={name}
+                        id={name}
+                        ref={register(getRegisterAttributes())}
+                    ></input>
+                    {renderIcons()}
+                    {displayMsg(name)}
+                </div>
+            </>
+        );
+    };
+
     return (
         <>
-            <label htmlFor={name}>{label}</label>
-            <input
-                type={type}
-                name={name}
-                id={name}
-                ref={register(getRegisterAttributes())}
-            ></input>
-
-            {displayMsg()}
+            {renderInput(name, label)}
 
             {description && (
                 <span className="cmp-form_description">{description}</span>
             )}
+
+            {renderMatch()}
         </>
     );
 };
--- waters-aem-frontend/src/forms/fields/patterns/index.js
+++ waters-aem-frontend/src/forms/fields/patterns/index.js
@@ -21,14 +21,97 @@ export const functions = {
             return true;
         }
     },
-    password: value => {
-        // add password validation logic
-        return true;
+    password: (value, ref, setError, clearError) => {
+        let validations = 0;
+        let errors = [];
+
+        // Check length (required)
+        if (value.length < 8) {
+            errors.push({
+                name: "shortPassword",
+                type: "invalidLength",
+                msg: "Password",
+                ref: ref
+            });
+        } else {
+            clearError("shortPassword");
+        }
+
+        // Check for lowercase
+        if (!test(value, /^.*[a-z]+.*$/)) {
+            errors.push({
+                name: "noLowercase",
+                type: "missingLowercase",
+                msg: "Password",
+                ref: ref
+            });
+        } else {
+            validations++;
+            clearError("noLowercase");
+        }
+
+        // Check for uppercase
+        if (!test(value, /^.*[A-Z]+.*$/)) {
+            errors.push({
+                name: "noUppercase",
+                type: "missingUppercase",
+                msg: "Password",
+                ref: ref
+            });
+        } else {
+            validations++;
+            clearError("noUppercase");
+        }
+
+        // Check for digit
+        if (!test(value, /^.*[0-9]+.*$/)) {
+            errors.push({
+                name: "noDigits",
+                type: "missingDigits",
+                msg: "Password",
+                ref: ref
+            });
+        } else {
+            validations++;
+            clearError("noDigits");
+        }
+
+        // Check for special character
+        if (!test(value, /^.*\W+.*$/)) {
+            errors.push({
+                name: "noSpecial",
+                type: "missingSpecial",
+                msg: "Password",
+                ref: ref
+            });
+        } else {
+            validations++;
+            clearError("noSpecial");
+        }
+
+
+        if (validations >= 3 && value.length >= 8) {
+            clearError("shortPassword");
+            clearError("noLowercase");
+            clearError("noUppercase");
+            clearError("noDigits");
+            clearError("noSpecial");
+            ref.classList.remove('error');
+            return true;
+        } else {
+            errors.forEach(error => {
+                setError(error.name, error.type, error.msg, error.ref);
+            });
+            return false;
+        }
     },
     email: value => {
         return test(
             value,
             /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         );
+    },
+    matching: (value, matchRef) => {
+        return value === matchRef.value;
     }
 };
