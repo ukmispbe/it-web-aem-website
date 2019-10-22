7e957c4a7d47e1576062b248e4c414f8a93a8447
Garcia, Josue <josue.garcia@icfnext.com>
2019-10-22 07:22:36 -0500

    BTWCT-1275: UI: Custom Event handling to Capture Analytics on Search page

--- waters-aem-frontend/src/scripts/analytics.js
+++ waters-aem-frontend/src/scripts/analytics.js
@@ -28,18 +28,58 @@ class Analytics {
     }
  
     buildModel = (name, model) => {
-        console.log('buildModel name:', name)
-        if (name == "stock" || name == "cart") { 
-            return ({
-                detail: {
-                    products: [model]
-                }
-            })
+        let returnModel = null;
+
+        switch (name) {
+            case "stock":
+            case "cart":
+                returnModel = this.mapAddToCartModel(model);
+                break;
+            case "search":
+                returnModel = this.mapSearchModel(model);
+                break;
+            default:
+                break;
+        }
+
+        return returnModel;
+    }
+
+    mapAddToCartModel = model => {
+        return {
+            detail: {
+                products: [model]
+            }
+        }
+    }
+
+    mapSearchModel = (model) => {
+        if (!model) {
+            return {};
+        }
+
+        const category = model.category ? model.category : '';
+        const contentType = model.content_type ? model.content_type : '';
+        const facetsObj = model.facets ? model.facets : {};
+        const facets = Object.entries(facetsObj).map(item => {
+            return {
+                name: item[0],
+                values: item[1],
+            }
+        });
+
+        return {
+            search: {
+                category,
+                contentType,
+                facets,
+                totalResults: model.total,
+            }
         }
     }
 
     dispatchEvent = (eventName, obj) => {
-        console.log('dispatched', eventName, obj)
+        console.log('dispatched', eventName, obj);
         document.dispatchEvent(new CustomEvent(eventName, obj));
     }
 }
