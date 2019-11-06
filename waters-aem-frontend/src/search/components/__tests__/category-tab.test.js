bf8191d0a4d1afb3e0ef07b98efbad10ace73239
Garcia, Josue <josue.garcia@icfnext.com>
2019-11-06 16:21:54 -0600

    Jest Unit Tests => CategoryTab React Component

---
+++ waters-aem-frontend/src/search/components/__tests__/category-tab.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import screenSizes from '../../../scripts/screenSizes';
+import CategoryTab from '../category-tab';
+
+describe('Feature: CategoryTab React Component', () => {
+    const props = {
+        name: 'Tab1',
+        count: 0,
+        isActive: false,
+        index: 0,
+        onClick: jest.fn(() => {})
+    };
+
+    describe('Scenario: Rendering', () => {
+        describe('When the screen size is tablet or mobile', () => {
+            it('Then it should not render anything and match snapshot', () => {
+                screenSizes.isTabletAndUnder = jest.fn(() => true);
+                const json = renderer.create(<CategoryTab {...props}/>);
+                expect(json).toMatchSnapshot();
+            });
+        });
+
+        describe('When the screen size is desktop and tab is inactive', () => {
+            it('Then is should render the component and match snapshot', () => {
+                screenSizes.isTabletAndUnder = jest.fn(() => false);
+                const json = renderer.create(<CategoryTab {...props}/>);
+                expect(json).toMatchSnapshot();
+            });
+        });
+
+        describe('When the screen size is desktop and tab is active', () => {
+            it('Then is should render the component and match snapshot', () => {
+                props.isActive = true;
+                screenSizes.isTabletAndUnder = jest.fn(() => false);
+                const json = renderer.create(<CategoryTab {...props}/>);
+                expect(json).toMatchSnapshot();
+            });
+        });
+    });
+
+    describe('Scenario: User Interaction', () => {
+        describe('When the tab is clicked', () => {
+            it('It should execute the onClick prop', () => {
+                screenSizes.isTabletAndUnder = jest.fn(() => false);
+                const onClickSpy = spyOn(props, 'onClick');
+                const wrapper = shallow(<CategoryTab {...props} />);
+                wrapper.find('.cmp-search__categories-tabs--tab').simulate('click');
+
+                expect(onClickSpy).toHaveBeenCalled();
+
+                onClickSpy.mockRestore();
+            });
+        });
+    });
+});
--- waters-aem-frontend/src/search/components/__tests__/no-results.test.js
+++ waters-aem-frontend/src/search/components/__tests__/no-results.test.js
@@ -1,17 +1,9 @@
-
 import React from 'react';
 import renderer from 'react-test-renderer';
 import NoResults from '../no-results';
 
 describe('Feature: NoResults React Component', () => {
     describe('Scenario: Rendering', () => {
-        describe('When the component renders without props', () => {
-            it('Then it should throw a TypeError exception', () => {
-                const mockConsoleError = jest.spyOn(console, 'error').mockImplementation(() => {});
-                expect(() => renderer.create(<NoResults />)).toThrow(TypeError);
-                mockConsoleError.mockRestore();
-            });
-        });
         describe('When the component renders with props', () => {
             it('Then the snapshot should match', () => {
                 const searchText = {
