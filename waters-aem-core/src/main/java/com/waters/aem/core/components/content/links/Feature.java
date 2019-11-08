e965655e91e2dc36e0f52cf038beaf67467d3a9a
Cole Calamos <cole.calamos@icfnext.com>
2019-11-08 14:01:50 -0600

    BTWCT-1808: Added toggle for sign in link styling

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/links/Feature.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/links/Feature.java
@@ -2,12 +2,14 @@ package com.waters.aem.core.components.content.links;
 
 import com.citytechinc.cq.component.annotations.DialogField;
 import com.citytechinc.cq.component.annotations.widgets.PathField;
+import com.citytechinc.cq.component.annotations.widgets.Switch;
 import com.citytechinc.cq.component.annotations.widgets.TextField;
 import com.icfolson.aem.library.api.link.Link;
 import com.icfolson.aem.library.models.annotations.LinkInject;
 import com.waters.aem.core.constants.WatersConstants;
 import com.waters.aem.core.utils.LinkUtils;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 
@@ -52,6 +54,13 @@ public class Feature {
     @LinkInject
     private Link linkPath;
 
+    @DialogField(fieldLabel = "Sign In Link?",
+            ranking = 6)
+    @Switch(offText = "No", onText = "Yes")
+    @Inject
+    @Default(booleanValues = false)
+    private Boolean signIn;
+
 
     public String getTitle() {
         return title;
@@ -71,6 +80,10 @@ public class Feature {
 
     public Link getLinkPath() {
         return linkPath;
+    }
+
+    public Boolean isSignIn() {
+        return signIn;
     }
 
     public Boolean isExternal() {
