d0bc289bc92590d99152a866956fb0347f9c063d
Cole Calamos <cole.calamos@icfnext.com>
2019-12-05 11:00:52 -0600

    [BTWCT-2398] Initial detail tiles work

---
+++ waters-aem-frontend/src/detail-tiles/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Tile from './views/tile';
+
+const DetailTiles = ({
+    title,
+    tiles,
+    canCreate,
+    icons
+}) => {
+
+};
+
+export default DetailTiles;
