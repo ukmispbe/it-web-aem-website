f6c38c7afff93cbae31b1fe3aa96808390214f21
Cole Calamos <cole.calamos@icfnext.com>
2019-12-10 12:31:09 -0600

    [BTWCT-2398] Refactored dropdown for use in other situations

---
+++ waters-aem-frontend/src/forms/fields/components/select.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useContext } from 'react';
+import ReactSelect, { components, createFilter } from "react-select";
+import ReactSVG from "react-svg";
+
+import { useFieldApi, useFormApi } from '../../form';
+import customStyles from "../styles/dropdown.scss";
+
+const DropdownIndicator = props => {
+    return (
+        <components.DropdownIndicator {...props}>
+            <ReactSVG src={props.theme.dropdownIndicator} />
+        </components.DropdownIndicator>
+    );
+};
+
+const Select = (props) => {
+    const { name, options, dropdownIndicator, placeholder } = useContext(useFieldApi);
+    const { triggerValidation } = useContext(useFormApi);
+
+    const setupOptions = (label, value) => ({ label: label, value: value });
+
+    const getOptions = () => {
+        switch (name) {
+            case "country":
+                return options.map(val => setupOptions(val.displayName, val.countryCode));
+            case "region":
+                return options.map(val => setupOptions(val.displayName, val.regionCode));
+            default:
+                return options.map(val => setupOptions(val.label, val.value));
+        }
+    };
+
+    return (
+        <ReactSelect
+            {...props}
+            options={getOptions()}
+            isSearchable={true}
+            styles={customStyles}
+            placeholder={placeholder}
+            classNamePrefix={"cmp-custom-dropdown"}
+            components={{ DropdownIndicator }}
+            theme={{ dropdownIndicator }}
+            filterOption={createFilter({ ignoreCase: true, trim: true, matchFrom: "start"})}
+            onBlur={() => triggerValidation({ name: name })}
+        />
+    );
+};
+
+export default React.memo(Select);
--- waters-aem-frontend/src/forms/fields/dropdown.js
+++ waters-aem-frontend/src/forms/fields/dropdown.js
@@ -1,108 +1,27 @@
-import React, { useState, useEffect, useContext } from "react";
-import Select, { components, createFilter } from "react-select";
-import ReactSVG from "react-svg";
+import React, { useContext } from "react";
+import { RHFInput } from 'react-hook-form-input';
 
-import DigitalData from "../../scripts/DigitalData";
-import customStyles from "./styles/dropdown.scss";
+import Select from './components/select';
 import DisplayMessage from "./components/displaymessage";
 import Icons from './components/icons';
 
 import { useFormApi, useFieldApi } from '../form';
 
-const DropdownIndicator = props => {
-    return (
-        <components.DropdownIndicator {...props}>
-            <ReactSVG src={props.theme.dropdownIndicator} />
-        </components.DropdownIndicator>
-    );
-};
-
-const getOptions = (opts, name) => {
-    let opt;
-    switch (name) {
-        case "country":
-            opt = opts.map(val => {
-                return {
-                    label: val.displayName,
-                    value: val.countryCode
-                };
-            });
-            break;
-    }
-
-    return opt;
-};
-
-const getDefault = (options, name) => {
-    let defaultValue;
-    const allOptions = getOptions(options, name);
-
-    switch (true) {
-        case DigitalData.country != DigitalData.globalExperience && name == "country":
-            const activeOption = allOptions.find(function (option) {
-                return option.value.toLowerCase() == DigitalData.country.toLowerCase();
-            });
-            defaultValue = activeOption ? activeOption : '';
-            break;
-        default:
-            defaultValue = '';
-    }
-
-    return defaultValue;
-}
-
 const Dropdown = ({}) => {
-    const { name, label, options, dropdownIndicator, placeholder, validation } = useContext(useFieldApi);
+    const { name, label, validation } = useContext(useFieldApi);
     const { register, setValue } = useContext(useFormApi);
-    const [selectValue, setSelect] = useState();
-    const [hasBlurred, setBlurred] = useState(false);
-    const [required] = useState(validation ? validation.required : false);
-
-    const handleChange = opt => {
-        setSelect(opt);
-        setValue(name, opt.value, true);
-    };
-    const filterConfig = {
-        ignoreCase: true,
-        trim: true,
-        matchFrom: "start"
-    };
-
-    useEffect(() => {
-        if (typeof selectValue == 'undefined') {
-            handleChange(getDefault(options,name))
-        }
-    }, [])
-
-    const handleBlur = () => !hasBlurred ? setBlurred(true) : null;
 
     return (
-        <div className={"cmp-form-field-dropdown--wrapper" + (hasBlurred ? " dirty" : "")}>
+        <div className="cmp-form-field-dropdown--wrapper">
             <label htmlFor={name}>{label}</label>
             <div className={"cmp-form-field-dropdown--wrapper"}>
-                <Select
-                    id={name}
+                <RHFInput
                     name={name}
-                    defaultValue={""}
-                    options={getOptions(options, name)}
-                    value={selectValue}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    isSearchable={true}
-                    styles={customStyles}
-                    placeholder={placeholder}
-                    classNamePrefix={"cmp-custom-dropdown"}
-                    components={{ DropdownIndicator }}
-                    theme={{ dropdownIndicator }}
-                    filterOption={createFilter(filterConfig)}
-                    ref={() =>
-                        register(
-                            { name },
-                            {
-                                validate: value => !required || !!value
-                            }
-                        )
-                    }
+                    as={<Select />}
+                    mode="onChange"
+                    rules={validation}
+                    register={register}
+                    setValue={setValue}
                 />
                 <Icons />
             </div>
