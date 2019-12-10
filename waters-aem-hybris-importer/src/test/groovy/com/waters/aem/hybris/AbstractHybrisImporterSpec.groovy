42091acde8d5762fea2ec5d9d529f81e4b56655f
LaPaglia <nick.lapaglia@icfolson.com>
2019-12-10 04:59:14 -0600

    [BTWCT-2323] Updated tests - created mock RolloutManager

--- waters-aem-hybris-importer/src/test/groovy/com/waters/aem/hybris/AbstractHybrisImporterSpec.groovy
+++ waters-aem-hybris-importer/src/test/groovy/com/waters/aem/hybris/AbstractHybrisImporterSpec.groovy
@@ -1,7 +1,5 @@
 package com.waters.aem.hybris
 
-import com.day.cq.wcm.api.WCMException
-import com.day.cq.wcm.msm.api.LiveRelationship
 import com.day.cq.wcm.msm.api.RolloutManager
 import com.icfolson.aem.library.core.specs.AemLibrarySpec
 import com.icfolson.aem.library.models.impl.EnumInjector
@@ -15,14 +13,10 @@ import com.waters.aem.hybris.client.MockHybrisClient
 import com.waters.aem.hybris.importer.HybrisCatalogImporter
 import com.waters.aem.hybris.importer.HybrisProductImporter
 import com.waters.aem.hybris.importer.impl.DefaultHybrisCatalogImporter
-import org.apache.sling.api.resource.ResourceResolver
 import com.waters.aem.hybris.importer.impl.DefaultHybrisProductImporter
+import com.waters.aem.hybris.mocks.MockRolloutManager
 import com.waters.aem.hybris.mocks.MockSkuRepository
-
 import spock.lang.Shared
-
-import javax.jcr.Node
-import javax.jcr.RepositoryException
 
 abstract class AbstractHybrisImporterSpec extends AemLibrarySpec {
 
@@ -37,62 +31,7 @@ abstract class AbstractHybrisImporterSpec extends AemLibrarySpec {
         slingContext.registerService(SkuRepository, new MockSkuRepository())
         slingContext.registerService(SiteRepository, new DefaultSiteRepository())
         slingContext.registerService(HybrisImporterAuditService, new MockHybrisImporterAuditService())
-        slingContext.registerService(RolloutManager, new RolloutManager() {
-            @Override
-            void rollout(RolloutManager.RolloutParams rolloutParams) throws WCMException {
-
-            }
-
-            @Override
-            void rollout(ResourceResolver resourceResolver, LiveRelationship liveRelationship, boolean b) throws WCMException {
-
-            }
-
-            @Override
-            void rollout(ResourceResolver resourceResolver, LiveRelationship liveRelationship, boolean b, boolean b1) throws WCMException {
-
-            }
-
-            @Override
-            void updateRolloutInfo(Node node, boolean b, boolean b1) throws WCMException {
-
-            }
-
-            @Override
-            boolean isExcludedProperty(String s) {
-                return false
-            }
-
-            @Override
-            boolean isExcludedProperty(boolean b, String s) {
-                return false
-            }
-
-            @Override
-            boolean isExcludedPageProperty(String s) {
-                return false
-            }
-
-            @Override
-            boolean isExcludedParagraphProperty(String s) {
-                return false
-            }
-
-            @Override
-            boolean isExcludedNodeType(String s) {
-                return false
-            }
-
-            @Override
-            boolean isExcludedNode(Node node) throws RepositoryException {
-                return false
-            }
-
-            @Override
-            boolean isReservedProperty(String s) {
-                return false
-            }
-        })
+        slingContext.registerService(RolloutManager, new MockRolloutManager())
 
         slingContext.registerInjector(new EnumInjector(), Integer.MIN_VALUE)
 
