f14ddde7d4f585c6d225c6153e57af0bbc957854
TylerTessmann-ICFOlson <tyler.tessmann@icf.com>
2019-12-30 10:24:36 -0600

    [BTWCT-2481] fixed bug in portal that prevented proper rendering

--- waters-aem-frontend/src/utils/modal/modal-portal.js
+++ waters-aem-frontend/src/utils/modal/modal-portal.js
@@ -1,19 +1,24 @@
-import React, { useEffect} from "react";
+import React, { useEffect, useState, useRef} from "react";
 import ReactDOM from "react-dom";
 
 const ModalPortal = props => {
     const modalElement = document.getElementById("modal-root");
-    const el = document.createElement("div");
+    const el = useRef( document.createElement('div'));
+    const [dynamic] = useState(!el.current.parentElement);
 
     useEffect(() => {
-        modalElement.appendChild(el);
+        if (dynamic) { 
+            modalElement.appendChild(el.current);
+        }
 
         return () => {
-            modalElement.removeChild(el);
+            if (dynamic && el.current.parentElement) {
+                modalElement.removeChild(el.current);
+            }
         };
     }, []);
 
-    return ReactDOM.createPortal(props.children, el);
+    return ReactDOM.createPortal(props.children, el.current);
 };
 
-export default ModalPortal;
+export default React.memo(ModalPortal);
