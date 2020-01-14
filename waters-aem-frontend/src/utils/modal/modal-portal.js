c2087f5e118c0ff9fcddf9ee60d315d76f6058db
Garcia, Josue <josue.garcia@icfnext.com>
2020-01-14 15:45:28 -0600

    BTWCT-2656: TD UI: Add Unit Tests for Modal Portal

--- waters-aem-frontend/src/utils/modal/modal-portal.js
+++ waters-aem-frontend/src/utils/modal/modal-portal.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState, useRef} from "react";
+import React, { useState, useRef} from "react";
 import ReactDOM from "react-dom";
 
 const ModalPortal = props => {
     const modalElement = document.getElementById("modal-root");
-    const el = useRef( document.createElement('div'));
-    const [dynamic] = useState(!el.current.parentElement);
+    const el = useRef(document.createElement('div'));
+    const [dynamic] = useState(!el.current.parentElement); 
 
-    useEffect(() => {
+    React.useEffect(() => {
         if (dynamic) { 
             modalElement.appendChild(el.current);
         }
