7e57de7878226b705c614b92691443545127b953
LaPaglia <nick.lapaglia@icfolson.com>
2020-01-23 10:00:33 -0600

    [BTWCT-2900] Added toggle to authoring dialog and method for getting update password endpoint

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/forms/ResetPassword.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/forms/ResetPassword.java
@@ -4,19 +4,23 @@ import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ExporterConstants;
 import com.citytechinc.cq.component.annotations.Component;
 import com.citytechinc.cq.component.annotations.DialogField;
+import com.citytechinc.cq.component.annotations.Option;
 import com.citytechinc.cq.component.annotations.widgets.PathField;
+import com.citytechinc.cq.component.annotations.widgets.Selection;
 import com.icfolson.aem.library.api.link.Link;
 import com.icfolson.aem.library.models.annotations.LinkInject;
 import com.waters.aem.core.constants.WatersConstants;
 import com.waters.aem.core.form.captcha.CaptchaService;
 import com.waters.aem.core.services.account.WatersAccountService;
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 
 import javax.annotation.Nonnull;
+import javax.inject.Inject;
 
 @Component(value = "Reset Password",
     description = "This is the Reset Password component for Waters site",
@@ -31,6 +35,10 @@ public class ResetPassword implements ComponentExporter {
 
     public static final String RESOURCE_TYPE = "waters/components/content/forms/resetpassword";
 
+    protected static final String RESET_OPTION = "reset";
+
+    protected static final String UPDATE_OPTION = "update";
+
     @OSGiService
     private WatersAccountService accountService;
 
@@ -44,12 +52,34 @@ public class ResetPassword implements ComponentExporter {
     @LinkInject
     private Link redirectLink;
 
+    @DialogField(fieldLabel = "Form Type",
+        fieldDescription = "Select the functional type for this form",
+        ranking = 2)
+    @Selection(
+    type = Selection.SELECT,
+    options = {
+            @Option(text = "Reset Password", value = RESET_OPTION),
+            @Option(text = "Update Password", value = UPDATE_OPTION)
+        }
+    )
+    @Inject
+    @Default(values =  RESET_OPTION)
+    private String formType;
+
     public Link getRedirectLink() {
         return redirectLink;
     }
 
+    public String getFormType() {
+        return formType;
+    }
+
     public String getChangePasswordUrl() {
         return accountService.getChangePasswordUrl();
+    }
+
+    public String getUpdatePasswordUrl() {
+        return accountService.getUpdatePasswordUrl();
     }
 
     public String getCaptchaSiteKey() {
