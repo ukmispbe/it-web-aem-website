fcc7dad7c488350faf3057a73617d147ff17e571
Aizhan Osmonova <aizhan_osmonova@waters.com>
2020-02-25 11:06:43 -0500

    [BTWCT-2514] Order details init

---
+++ waters-aem-frontend/src/order-details/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OrderDetails = ( props ) => {
+
+    return (
+        <div className="cmp-order-details__container">
+            Test
+        </div>
+    )
+}
+
+export default OrderDetails;
