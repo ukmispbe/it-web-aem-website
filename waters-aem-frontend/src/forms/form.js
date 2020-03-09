59e882b985f4cb1231a63fc461968e201e6bf2cd
Aizhan Osmonova <aizhan_osmonova@waters.com>
2020-03-09 10:40:03 -0400

    [BTWCT-3526] Clean up

--- waters-aem-frontend/src/forms/form.js
+++ waters-aem-frontend/src/forms/form.js
@@ -59,14 +59,14 @@ const Form = ({
 
     const checkIfDisabled = () => {
         const requiredFields = config.fields
-            .filter((field) => ('validation' in field && field.validation.required === true && ('active' in field ? field.active === true : true)));
+            .filter(field => ('validation' in field && field.validation.required === true && ('active' in field ? field.active === true : true)));
         const values = getValues();
         const emptyRequiredFields = requiredFields
             .filter(field => {
-            return values[field.name]==="" || values[field.name]==false || values[field.name]==null || values[field.name]==undefined
+            return values[field.name] === "" || values[field.name] === false || values[field.name] === null || values[field.name] === undefined
             });
-        const isConfirmPasswordFieldEmpty = 'confirmPassword' in values ? values['confirmPassword']==="" : false;
-        return (emptyRequiredFields.length!== 0 || isConfirmPasswordFieldEmpty || Object.keys(errors).length>0);
+        const isConfirmPasswordFieldEmpty = 'confirmPassword' in values ? values['confirmPassword'] === "" : false;
+        return (emptyRequiredFields.length !== 0 || isConfirmPasswordFieldEmpty || Object.keys(errors).length>0);
     };
 
     const cancelHandler = clear => {
