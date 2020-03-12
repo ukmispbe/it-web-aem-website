cce76d29888b450c796f55b39d1c048544b15700
Sarah Dulaney <Sarah_Dulaney@waters.com>
2020-03-12 11:40:02 -0400

    BTWCT-3515: java and service changes

--- waters-aem-frontend/src/detail-tiles/hooks/useProfile.js
+++ waters-aem-frontend/src/detail-tiles/hooks/useProfile.js
@@ -3,30 +3,29 @@ import generateTiles from '../utils/generateTiles';
 import UserDetailsLazy from '../../my-account/services/UserDetailsLazy';
 import SoldToDetailsLazy from '../../my-account/services/SoldToDetailsLazy';
 
-export default (fetchEndPoint, type, icon) => {
+export default (fetchDetailsEndPoint, fetchSoldToEndPoint, type, icon) => {
     const [data, setData] = useState();
     const [tiles, setTiles] = useState([]);
     const useDefaultSoldTo = true;
     useEffect(() => {
-
         switch (type) {
             case 'personal':
-                UserDetailsLazy(fetchEndPoint).then(response => {
+                UserDetailsLazy(fetchDetailsEndPoint).then(response => {
                     setData(response);
                 });
                 break;
             case 'password':
-                UserDetailsLazy(fetchEndPoint).then(response => {
+                UserDetailsLazy(fetchDetailsEndPoint).then(response => {
                     setData(response);
                 });
                 break;
             case 'shipping':
-                SoldToDetailsLazy(fetchEndPoint, useDefaultSoldTo).then(response => {
+                SoldToDetailsLazy(fetchSoldToEndPoint, useDefaultSoldTo).then(response => {
                     setData(response);
                 });
                 break;
             case 'billing':
-                SoldToDetailsLazy(fetchEndPoint, useDefaultSoldTo).then(response => {
+                SoldToDetailsLazy(fetchSoldToEndPoint, useDefaultSoldTo).then(response => {
                     setData(response);
                 });
                 break;
