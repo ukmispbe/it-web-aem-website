c5feffff9b8e7b9869dfc5a4d1a2749138da09ca
Aizhan Osmonova <aizhan_osmonova@waters.com>
2020-03-13 15:40:33 -0400

    [BTWCT-3510] Update routes

--- waters-aem-frontend/src/my-account/index.js
+++ waters-aem-frontend/src/my-account/index.js
@@ -10,15 +10,15 @@ const MyAccountRouter = (props) => {
     return (
         <HashRouter hashType={"noslash"}>
             <Switch>
-                <Route exact path={routes.myAccount}>
+                <Route exact path={routes.myAccount.path}>
                     <MyAccount {...props} />
                 </Route>
-                <Route exact path={routes.profile}>
+                <Route exact path={routes.profile.path}>
                     <Aside tiles={props.tiles}>
                         <MyProfile configs={props.myProfile} />
                     </Aside>
                 </Route>
-                <Route exact path={routes.changePassword}>
+                <Route exact path={routes.changePassword.path}>
                     <Aside tiles={props.tiles}>
                         <ChangePassword configId={props.changePassword.config} />
                     </Aside>
--- waters-aem-frontend/src/my-account/routes.js
+++ waters-aem-frontend/src/my-account/routes.js
@@ -1,5 +1,25 @@
 export default {
-    myAccount: "/",
-    profile: "/profile",
-    changePassword: "/changepassword"
+    myAccount: {
+        path: "/",
+    }
+    profile: {
+        path: "/profile",
+        link: "Profile",
+        breadcrumbs: ['/']
+    }
+    changePassword: {
+        path: "/changepassword",
+        label: "Change password",
+        breadcrumbs: ['/']
+    },
+    orderHistory: {
+        path: "/orderhistory",
+        label: "Order History",
+        breadcrumbs: ['/']
+    },
+    orderDetails: {
+        path: "/orderdetails",
+        label: "Order Details",
+        breadcrumbs: ['/', '/orderhistory']
+    }
 };
