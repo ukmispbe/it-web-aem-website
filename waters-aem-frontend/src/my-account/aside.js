06cff200a4de0d4661a43398953ed8e9c8ab4c54
Aizhan Osmonova <aizhan_osmonova@waters.com>
2020-03-17 18:00:04 -0400

    [BTWCT-3510] Cleanup

--- waters-aem-frontend/src/my-account/aside.js
+++ waters-aem-frontend/src/my-account/aside.js
@@ -1,10 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router-dom";
 import Title from "../typography/title";
 import Ecommerce from "../scripts/ecommerce";
-import routes from "./routes";
 import Breadcrumb from "./components/breadcrumb";
 
 const Aside = props => {
