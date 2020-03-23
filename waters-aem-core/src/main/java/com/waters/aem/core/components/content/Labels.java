2ee4212ea5927ba5b28d2a8f3a9fa7a38f30be92
conssha <sachin.sharma5@publicissapient.com>
2020-03-23 17:50:02 +0530

    Component name updated and code refactored

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/Labels.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/Labels.java
@@ -19,6 +19,8 @@ import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import javax.annotation.PostConstruct;
@@ -27,8 +29,8 @@ import java.util.*;
 
 import static com.icfolson.aem.library.core.constants.ComponentConstants.*;
 
-@Component(value = "Cart",
-        description = "This is the Cart component for Waters site my account page",
+@Component(value = "Labels",
+        description = "This is the Labels component for Waters site",
         listeners = {
                 @Listener(name = EVENT_AFTER_EDIT, value = REFRESH_PAGE),
                 @Listener(name = EVENT_AFTER_MOVE, value = REFRESH_PAGE),
@@ -40,20 +42,22 @@ import static com.icfolson.aem.library.core.constants.ComponentConstants.*;
         }
 )
 @Model(adaptables = SlingHttpServletRequest.class,
-        adapters = {Cart.class, ComponentExporter.class},
-        resourceType = Cart.RESOURCE_TYPE,
+        adapters = {Labels.class, ComponentExporter.class},
+        resourceType = Labels.RESOURCE_TYPE,
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME,
         extensions = ExporterConstants.SLING_MODEL_EXTENSION)
-public class Cart implements ComponentExporter {
+public class Labels implements ComponentExporter {
 
-    public static final String RESOURCE_TYPE = "waters/components/content/cart";
+    public static final String RESOURCE_TYPE = "waters/components/content/labels";
+
+    private static final Logger LOG = LoggerFactory.getLogger(Labels.class);
 
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
-    public static final String PROPERTY_LABELS_JSON = "cart-labels";
+    public static final String PROPERTY_LABELS_JSON = "labels-json";
 
-    public static final String PROPERTY_CONFIGS_JSON = "cart-configs";
+    public static final String PROPERTY_CONFIGS_JSON = "configs-json";
     
     @Inject
     private Resource resource;
@@ -81,8 +85,8 @@ public class Cart implements ComponentExporter {
 
 
     @PostConstruct
-    protected void updateNode() {
-        if (resource.isResourceType(Cart.RESOURCE_TYPE)) {
+    protected void updateOrCreateJson() {
+        if (resource.isResourceType(Labels.RESOURCE_TYPE)) {
             try {
                 ModifiableValueMap modifiableValueMap = resource.adaptTo(ModifiableValueMap.class);
                 Iterator<JsonFields> labelsItr = getLabels().iterator();
@@ -101,7 +105,7 @@ public class Cart implements ComponentExporter {
                 modifiableValueMap.put(PROPERTY_CONFIGS_JSON, jsonMap.size() > 0 ? MAPPER.writeValueAsString(jsonMap) : "");
                 resource.getResourceResolver().commit();
             } catch (Exception e) {
-                e.printStackTrace();
+                LOG.error("Exception occurred while working on JSON properties: "+e.getMessage());
             }
         }
     }
