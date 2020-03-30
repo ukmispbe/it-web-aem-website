ad9ebec36d9a359ae9b7a37b506e3990798ea4dd
conssha <sachin.sharma5@publicissapient.com>
2020-03-30 17:18:44 +0530

    SSL Context Added

--- waters-aem-core/src/main/java/com/waters/aem/core/servlets/WatersContentService.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/servlets/WatersContentService.java
@@ -10,9 +10,12 @@ import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.util.EntityUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
@@ -25,8 +28,14 @@ import org.osgi.service.component.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.security.cert.CertificateException;
+import javax.security.cert.X509Certificate;
 import javax.servlet.Servlet;
 import java.net.HttpURLConnection;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -39,10 +48,10 @@ public class WatersContentService extends SlingAllMethodsServlet {
     private static final Logger LOG = LoggerFactory.getLogger(WatersContentService.class);
 
     @Reference
-    private Externalizer externalizer;
+    private transient Externalizer externalizer;
 
     @Reference
-    private SlingSettingsService settingsService;
+    private transient SlingSettingsService settingsService;
 
     @Override
     public void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) {
@@ -64,13 +73,12 @@ public class WatersContentService extends SlingAllMethodsServlet {
                     response.getWriter().write("Content Service is supposed to be executed on Publish instance only");
                 }
             } else {
-                response.getWriter().write("Add correct parameters: \n path = Page path which exists on AEM \n page = Response level \"full\" to get Navigation content along with page content");
+                response.getWriter().write("Add correct parameters: \n pagePath = Page path which exists on AEM \n depth = Response level \"full\" to get Navigation content along with page content");
             }
         } catch (Exception e) {
             LOG.error("Exception Occurred: {}", e.getMessage());
             LOG.debug("Exception Occurred: {}", e);
         }
-
     }
 
     private final String buildJsonForPage(final String path, final String responseLevel, final SlingHttpServletRequest request) {
@@ -84,14 +92,22 @@ public class WatersContentService extends SlingAllMethodsServlet {
                 //TODO:USE CompletableFuture API instead of sequence
                 pagePublishCaaSUrl = externalizer.publishLink(resourceResolver, path.concat(".caas.infinity.json")).replace(WatersConstants.CUSTOM_ROOT_PATH, WatersConstants.ROOT_PATH);
                 pageJsonResponse = getHttpResponseAsStringForURI(pagePublishCaaSUrl);
-                if (StringUtils.isNoneBlank(responseLevel) && responseLevel.equalsIgnoreCase("full")) {
+                if (StringUtils.isBlank(pageJsonResponse)) {
+                    pageJsonResponse = "\"\"";
+                }
+                if (StringUtils.isNotBlank(responseLevel) && responseLevel.equalsIgnoreCase("full")) {
                     final Resource resource = resourceResolver.getResource(path + "/jcr:content/header/par/navigation");
                     if (null != resource) {
                         navigationCompJsonResponse = getHttpResponseAsStringForURI(pagePublishCaaSUrl.replace(".caas.infinity.json", "/_jcr_content/header/par/navigation.model.json"));
                         LOG.debug("JSON Content- {\"Page Content\": {} ,\"Navigation Content\": {} }", pageJsonResponse, navigationCompJsonResponse);
                         LOG.info("JSON returned with full page content for: {}", path);
                         LOG.info("JSON fetched from AEM in {} ms", stopwatch.elapsed(TimeUnit.MILLISECONDS));
-                        return "{\"Page Content\":" + pageJsonResponse + ",\"Navigation Content\":" + navigationCompJsonResponse + "}";
+                        if (StringUtils.isNotBlank(navigationCompJsonResponse)) {
+                            return "{\"Page Content\":" + pageJsonResponse + ",\"Navigation Content\":" + navigationCompJsonResponse + "}";
+                        } else {
+                            LOG.info("Navigation node exist but it doesn't return any content through model.json call");
+                            return "{\"Page Content\":" + pageJsonResponse + ",\"Navigation Content\":" + "\"\"" + "}";
+                        }
                     }
                 }
             } else {
@@ -106,25 +122,35 @@ public class WatersContentService extends SlingAllMethodsServlet {
         return "{\"Page Content\":" + pageJsonResponse + "}";
     }
 
-    private final String getHttpResponseAsStringForURI(final String URI) {
+    private final String getHttpResponseAsStringForURI(final String URI) throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
         String responseEntity = "";
-        LOG.debug("Making HTTP call to get the JSON for URI: {}", URI);
+        LOG.debug("Making HTTP/HTTPS call to get the JSON for URI: {}", URI);
         final HttpUriRequest request = RequestBuilder.get(URI)
                 .addHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType())
                 .addHeader(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON.getMimeType())
                 .build();
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+        SSLContextBuilder builder = new SSLContextBuilder();
+        builder.loadTrustMaterial(new TrustSelfSignedStrategy() {
+            public boolean isTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                return true;
+            }
+        });
+        @SuppressWarnings("deprecation")
+        HostnameVerifier hostnameVerifier = SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build(), hostnameVerifier);
+        try (CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build()) {
             CloseableHttpResponse response = httpClient.execute(request);
             final StatusLine statusLine = response.getStatusLine();
             if (statusLine.getStatusCode() != HttpURLConnection.HTTP_OK) {
                 throw new HttpResponseException(statusLine.getStatusCode(), statusLine.getReasonPhrase());
             }
             responseEntity = EntityUtils.toString(response.getEntity(), "UTF-8");
+            LOG.debug("HTTP response for URI {} is: {}", URI, responseEntity);
         } catch (Exception e) {
             LOG.info("Exception occured: {}", e.getMessage());
             LOG.debug("Exception occured: {}", e);
         }
-        LOG.debug("Http call successful for uri: {} ", URI);
+        LOG.info("HTTP call successful for uri: {} ", URI);
         return responseEntity;
     }
 
