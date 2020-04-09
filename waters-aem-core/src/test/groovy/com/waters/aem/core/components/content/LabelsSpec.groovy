283d31cc6e8715b1e652fd8755fbec65cf2b6b6b
conssha <sachin.sharma5@publicissapient.com>
2020-04-09 22:32:58 +0530

    Code refactored and Unit test groovy script added

--- waters-aem-core/src/test/groovy/com/waters/aem/core/components/content/LabelsSpec.groovy
+++ waters-aem-core/src/test/groovy/com/waters/aem/core/components/content/LabelsSpec.groovy
@@ -1,6 +1,8 @@
 package com.waters.aem.core.components.content;
 
-import com.waters.aem.core.WatersSpec;
+import com.waters.aem.core.WatersSpec
+import com.waters.aem.core.components.content.links.BasicLink
+import com.waters.aem.core.components.content.links.JsonFields;
 
 class LabelsSpec extends WatersSpec {
 
@@ -9,19 +11,55 @@ class LabelsSpec extends WatersSpec {
             waters {
                 one {
                     "jcr:content" {
-                        labels()
-                    }
-                }
-                two {
-                    "jcr:content" {
-                        labels(
-                               
-                        )
+                        labels() {
+                            labelList {
+                                item1(label: "addToCart", labelText: "Add to Cart")
+                                item2(label: "orderHistory", labelText: "Order History")
+                            }
+                            configList {
+                                item1(link: "/content/waters/us/en/account/addToCart", text: "addToCartLink")
+                                item2(link: "/content/waters/us/en/account/switchCart", text: "switchCartLink")
+                            }
+                        }
+
                     }
                 }
             }
         }
+        slingContext.addModelsForClasses(JsonFields)
+        slingContext.addModelsForClasses(BasicLink)
     }
 
+    def "get labels"() {
+        setup:
+        def labelsSpec = requestBuilder.build {
+            path = "/content/waters/one/jcr:content/labels"
+        }.adaptTo(Labels)
+
+        expect:
+        labelsSpec.labelList.size() == 2
+
+        and:
+        labelsSpec.labelList.label == ["addToCart", "orderHistory"]
+
+        and:
+        labelsSpec.labelList.labelText == ["Add to Cart", "Order History"]
+    }
+
+    def "get configs"() {
+        setup:
+        def configsSpec = requestBuilder.build {
+            path = "/content/waters/one/jcr:content/labels"
+        }.adaptTo(Labels)
+
+        expect:
+        configsSpec.configList.size() == 2
+
+        and:
+        configsSpec.configList.text == ["addToCartLink", "switchCartLink"]
+
+        and:
+        configsSpec.configList.link.path == ["/content/waters/us/en/account/addToCart", "/content/waters/us/en/account/switchCart"]
+    }
 }
 
