129e757ee968a7476f51d392e70f4a4c088b7046
Aizhan Osmonova <aizhan_osmonova@waters.com>
2020-04-15 16:18:16 -0400

    [BTWCT-2514] Unit testing

---
+++ waters-aem-frontend/src/order-details/__tests__/orderDetails-no-order-found.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
+import OrderDetails from '../index';
+import ErrorBoundary from '../../search';
+import { getOrderDetails } from '../orderDetails.services';
+import props from '../__mocks__/en_US/index';
+import { orderDetailsJSON } from '../__mocks__/en_US/services-json.test';
+import Spinner from "../../utils/spinner";
+
+describe('Feature: Order Details Component', () => {
+
+    beforeAll(() => {
+        delete window.location;
+        window.location = new URL('https://www.waters.com/my-account.html#orderdetails?id=15740002');
+        window.scrollTo = jest.fn();
+    });
+
+    afterAll(() => {
+        jest.mockRestoreAll();
+    });
+
+    describe('Scenario: Rendering', () => {
+        const wrapper = shallow(<OrderDetails {...props} />);
+
+        describe("When component is mounted", () => {
+
+            it("It should get order id from url", () => {
+                expect(window.location.hash).toEqual("#orderdetails?id=15740002");
+            });
+        })
+
+        describe("When no order is found", () => {
+            window.fetch = jest.fn(() => {
+                return {
+                    status: 704,
+                    json: async () => {}
+                }
+            });
+
+            it("Then it should match snapshot", () => {
+                const json = renderer.create(<OrderDetails {...props} />);
+                expect(json).toMatchSnapshot();
+            });
+        })
+    })
+})
