5989266f4f1265a81969f7a0309ac2d4c4c794b5
Nakirikanti <santosh_nakirikanti@waters.com>
2020-05-13 11:55:48 -0400

    BTWCT-4224: Check for account-tile.

--- waters-aem-frontend/src/detail-tiles/index.js
+++ waters-aem-frontend/src/detail-tiles/index.js
@@ -101,11 +101,13 @@ const DetailTiles = ({
         }
 
         return tiles.map((tile, key) => {
+           if(tile.name === 'personalDetailsTile'){
             const mailingAddress = tile.defaultValues.userAddress && tile.defaultValues.userAddress.filter(address => address.addressType === 'mailingAddress');
             const userCountry = mailingAddress.length ? mailingAddress[0].countryCode.toLowerCase() : '';
-            if(tile.name === 'personalDetailsTile' && (userCountry === 'kr' || userCountry === 'jp' || userCountry === 'tw' || userCountry === 'cn')) {
+            if(userCountry === 'kr' || userCountry === 'jp' || userCountry === 'tw' || userCountry === 'cn') {
                 swapFirstAndLastNames();
             }
+        }
 
             return <ErrorBoundary>
                     <Tile
