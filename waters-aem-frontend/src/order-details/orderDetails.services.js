9f9e1af5ed8a51cd3ebff31bc7129f0e6868e34a
Sarah Dulaney <Sarah_Dulaney@waters.com>
2020-05-14 12:28:28 -0400

    remove notes and logs

--- waters-aem-frontend/src/order-details/orderDetails.services.js
+++ waters-aem-frontend/src/order-details/orderDetails.services.js
@@ -25,7 +25,6 @@ export const getOrderDetails = async (endpoint, id, setError) => {
 }
 
 const buildSearchURL = (endpoint, lineItems, isocode) => {
-    console.log("buildSearchURL endpoint", endpoint);
     let skus, rows, keywords, url;
     skus = lineItems.map(lineItem => lineItem.materialNumber);
     rows = skus.length;
@@ -34,7 +33,6 @@ const buildSearchURL = (endpoint, lineItems, isocode) => {
 }
 
 export const getItemDetails = async (endpoint, lineItems, setError, isocode) => {
-    console.log("getItemDetails endpoint", endpoint);
     const url = buildSearchURL(endpoint, lineItems, isocode);
     const response = await getData(url);
     const responseBody = await response.json();
@@ -45,18 +43,6 @@ export const getItemDetails = async (endpoint, lineItems, setError, isocode) => 
         setError({status: response.status, code: responseBody.code});
     }
 }
-
-//https://test-www.waters.com:8443/api/waters/search/category_facet$shop:Shop?isocode=en_US&keyword=176001375%20176001556%20176001557&multiselect=true&page=1&rows=100&sort=most-relevant
-
-// get order details data
-// get list of skus and count
-// build search query
-// execute search query
-// merge api results with OD lineItems data
-// push to shipment component
-// push to sku component
-// componentDidUpdate 
-
 
 export const matchLineItems = (orderDetailsAPIResults, searchAPIResults) => {
     orderDetailsAPIResults.lineItems.forEach(soldItem => {
