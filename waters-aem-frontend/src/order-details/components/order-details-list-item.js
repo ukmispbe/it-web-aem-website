861a6a9c5d7bd984e80d9b3d76600748c4e546bd
Sarah Dulaney <Sarah_Dulaney@waters.com>
2020-05-15 10:42:55 -0400

    BTWCT-3226: PR comment updates

--- waters-aem-frontend/src/order-details/components/order-details-list-item.js
+++ waters-aem-frontend/src/order-details/components/order-details-list-item.js
@@ -3,10 +3,6 @@ import React from 'react';
 class OrderDetailsListItem extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            userCountry: this.props.skuConfig.countryCode
-        };
     }
 
     render() {
