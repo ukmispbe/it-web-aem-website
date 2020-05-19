34dc1cfb7cc6017a13e86c075c7b182b5199a989
Aizhan Osmonova <aizhan_osmonova@waters.com>
2020-05-18 22:32:59 -0400

    BTWCT-4224 Get, set, remove guid in local storage

--- waters-aem-frontend/src/stores/localStore.js
+++ waters-aem-frontend/src/stores/localStore.js
@@ -1,8 +1,12 @@
 const keys = {
     guid: 'waters.guid',
+    cartId: 'waters.cartId'
 }
 
 const LocalStore = function () {
+    this.setGUID = value => window.localStorage.setItem(keys.guid, value.toString());
+    this.getGUID = () => window.localStorage.getItem(keys.guid);
+    this.removeGUID = () => window.localStorage.removeItem(keys.guid);
     this.setCartId = value => window.localStorage.setItem(keys.cartId, value.toString());
     this.getCartId = () => window.localStorage.getItem(keys.cartId);
     this.removeCartId = () => window.localStorage.removeItem(keys.cartId);
