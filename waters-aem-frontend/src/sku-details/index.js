aef154a8a53a676fde262db8c8740f9a292d620c
Paulus001 <paul.beard007@googlemail.com>
2020-07-14 11:41:59 +0100

    BTWCT-5744-ui-test-2-extra-space-and-misalignment-in-sku-list-and-sku-details Made changes to the SKU Page and added styling for when the Sign In Component is not displayed. Used a Div with class name cmp-sku-signin-wrapper-not-displayed.

--- waters-aem-frontend/src/sku-details/index.js
+++ waters-aem-frontend/src/sku-details/index.js
@@ -290,7 +290,8 @@ class SkuDetails extends React.Component {
                                 signInText1={this.state.skuInfo.signInText1}
                                 signInText2={this.state.skuInfo.signInText2}
                                 signInText3={this.state.skuInfo.signInText3}
-                            />)}
+                            />) 
+                            || LoginStatus.state() && (<div className="cmp-sku-signin-wrapper-not-displayed"></div>)}
                         {this.renderBuyInfo()}
                     </>;
             } else {
