9d6bdc7d56b6cb97275cb1a67e9be72aaf51cbe0
Ankit Upadhyay <ankit.upadhyay@publicissapient.com>
2020-07-15 21:50:14 +0530

    fix(): update punchout setup default service url

--- waters-aem-core/src/main/java/com/waters/aem/core/services/account/WatersPunchoutServiceConfiguration.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/services/account/WatersPunchoutServiceConfiguration.java
@@ -7,7 +7,7 @@ import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 public @interface WatersPunchoutServiceConfiguration {
 
     @AttributeDefinition(name = "Punchout Setup Url", description = "Url for setting up a Punchout")
-    String setupUrl() default "https://test-www.waters.com:8443/api/waters/punchout/v1/setup";
+    String setupUrl() default "https://api-sbox.waters.com/dev-waters-punchout-exp-api-v1/api/users/{userId}/punchout/setup/{sid}";
 
     @AttributeDefinition(name = "Punchout Login Url", description = "Url for Logging a Punchout user")
     String punchoutLogin() default "https://test-www.waters.com:8443/api/waters/punchout/v1/login";
