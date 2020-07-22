05e44531306832b07e4f67653a3688d24c0f8804
Sarthak Kakkar <sarkakka@publicisgroupe.net>
2020-07-22 11:43:09 +0530

    BTWCT-6043: Added as per review comments.

--- waters-aem-ui.apps/src/main/scripts/groovy/upgrade/Tags.groovy
+++ waters-aem-ui.apps/src/main/scripts/groovy/upgrade/Tags.groovy
@@ -2,7 +2,7 @@
 // Update code references
 move  "/etc/tags/waters" to "/content/cq:tags/waters"
 //If waters-ecommerce node is present under /etc/tags, on the env.
-move  "/etc/tags/waters-ecommerce" to "/etc/tags/waters-ecommerce"
+move  "/etc/tags/waters-ecommerce" to "/content/cq:tags/waters-ecommerce"
 //If we-retail-client-app node is present under /etc/tags, on the env.
 move  "/etc/tags/we-retail-client-app" to "/content/cq:tags/we-retail-client-app"
 getNode("/etc/tags").remove()
