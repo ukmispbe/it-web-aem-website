97c1083e75842cfdb9596bfd78b2ae63122cbef2
Ankit Upadhyay <ankit.upadhyay@publicissapient.com>
2020-08-13 21:39:55 +0530

    fix | fix modal on search page

--- waters-aem-frontend/src/my-account-dropdown/index.js
+++ waters-aem-frontend/src/my-account-dropdown/index.js
@@ -382,7 +382,6 @@ class MyAccountDropDown extends React.Component {
                     country: response.country,
                 });
                 (new LocalStore()).setCartId(response.cartId);
-                window.onbeforeunload = e => sessionStore.removePunchoutSetupDetails();
             }
         } else if (Object.keys(sessionStore.getPunchoutSetupDetails()).length === 0) {
             const { requestFailureTitle, requestFailureMessage } = this.props.eProcSetupFailure;
