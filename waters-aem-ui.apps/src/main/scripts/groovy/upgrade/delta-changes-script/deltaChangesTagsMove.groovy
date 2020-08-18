07c40c5da1a92aa8d13f03f4eb5ef117932c198b
grvmishra556 <67579313+grvmishra556@users.noreply.github.com>
2020-08-18 17:31:07 +0530

    Removing extra line from script

--- waters-aem-ui.apps/src/main/scripts/groovy/upgrade/delta-changes-script/deltaChangesTagsMove.groovy
+++ waters-aem-ui.apps/src/main/scripts/groovy/upgrade/delta-changes-script/deltaChangesTagsMove.groovy
@@ -1,6 +1,5 @@
 getNode("/content/cq:tags/waters").remove()
 save()
 move "/etc/tags/waters" to "/content/cq:tags/waters"
-move "/etc/tags/waters" to "/content/cq:tags/waters"
 getNode("/etc/tags").remove()
 save()
