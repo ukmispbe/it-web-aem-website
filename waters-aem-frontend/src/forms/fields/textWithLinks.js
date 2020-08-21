652ca98489c9184f80fce0ecab2b97d6b10e5abc
Iphtekhar Ahmad <iphtekhar.k@publicissapient.com>
2020-08-21 15:38:09 +0530

    Added default id

--- waters-aem-frontend/src/forms/fields/textWithLinks.js
+++ waters-aem-frontend/src/forms/fields/textWithLinks.js
@@ -29,7 +29,7 @@ const TextWithLinks = ({}) => {
                 config.length > 0 && (
                     <div className={`cmp-form-field-${type}--${name} ` + (addClass ? addClass : '')}>
                         {config.map((block, index) => {
-                            let itemToRender = block.type === "link" ? renderLink({...block, className: block.className || '', title: block.title || '', id: block.id || ''}) : renderText(block);
+                            let itemToRender = block.type === "link" ? renderLink({...block, className: block.className || '', title: block.title || '', id: block.id || `text-with-link-${index}`}) : renderText(block);
                             let space="";
 
                             if(block.rightSpace !== "false" || typeof block.rightSpace == "undefined") {
