02d9afa579d5511bd63e27f815799b25be881d6a
Ankit Upadhyay <ankit.upadhyay@publicissapient.com>
2020-08-25 14:47:11 +0530

    [BTWCT-6727] refactor | update registration form authorable field heading

--- waters-aem-core/src/main/java/com/waters/aem/core/components/content/forms/Registration.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/components/content/forms/Registration.java
@@ -96,7 +96,7 @@ public class Registration implements ComponentExporter {
     @LinkInject
     private Link redirectLink;
 
-    @DialogField(fieldLabel = "Redirect Link",
+    @DialogField(fieldLabel = "Redirect Link (eProc User)",
             fieldDescription = "Select or enter the redirect link URL. The destination page the user will be redirected " +
                     "to after successful submission",
             required  = true,
