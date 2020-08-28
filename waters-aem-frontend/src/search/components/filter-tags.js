8855fa51115b5352503df9157af0a937f9f89700
mehran <mehran.ahmed@publicissapient.com>
2020-08-28 16:00:05 +0530

    BTWCT-6675-search-data-locators

--- waters-aem-frontend/src/search/components/filter-tags.js
+++ waters-aem-frontend/src/search/components/filter-tags.js
@@ -83,7 +83,7 @@ const ContentTypeTag = props => {
 const ClearAllTag = props => {
     return <a href="javascript:void(0);"
             className="cmp-search-filters__tags__clear"
-            data-locator="link-search-filters-tags-clear"
+            data-locator="link-search-filters-tag-clear"
             onClick={props.onRemove}>
             <ReactSVG src={props.text.closeIcon} />
             <span>{props.text.clearAllFilters}</span>
