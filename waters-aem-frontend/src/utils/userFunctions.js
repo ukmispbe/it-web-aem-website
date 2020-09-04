4f59caee6d661916ff3111c24a345a1bb9f44163
Sarah Dulaney <Sarah_Dulaney@waters.com>
2020-09-04 07:50:55 -0400

    BTWCT-7085: update to regex

--- waters-aem-frontend/src/utils/userFunctions.js
+++ waters-aem-frontend/src/utils/userFunctions.js
@@ -114,7 +114,7 @@ const capitalize = str => {
 
 export const trimAndCapitalize = (item) => {
     if (item && typeof item === "string") {
-        item = item.replace("\\s","");
+        item = item.replace(/\s\s+/g, ' ');
         item = item.trim();
         item = capitalize(item);
     }
