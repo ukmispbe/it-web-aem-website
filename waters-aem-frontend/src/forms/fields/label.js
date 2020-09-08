005ceabcd41d4b2cf8ca667064448a8f978645f7
Paulus001 <paul.beard007@googlemail.com>
2020-09-08 14:42:16 +0100

    BTWCT-7227-ui-required-field-indicators-all-forms-my-account Removed name

--- waters-aem-frontend/src/forms/fields/label.js
+++ waters-aem-frontend/src/forms/fields/label.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Label = ({ addClass, label, name, htmlFor ="" }) => {
+const Label = ({ addClass, label, htmlFor ="" }) => {
     return (
         <label className={addClass} htmlFor={htmlFor}>
             {label} 
