56d0dfa84a4a44a1c7d08d220013d10dc65b26d6
Sarthak Kakkar <sarkakka@publicisgroupe.net>
2020-09-21 16:02:51 +0530

    Added date check.

--- waters-aem-ui.apps/src/main/scripts/groovy/upgrade/ActivateContent.groovy
+++ waters-aem-ui.apps/src/main/scripts/groovy/upgrade/ActivateContent.groovy
@@ -1,11 +1,25 @@
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+def dateInstanceWentDown = "" //Example -> 2020-09-14
+
 getPage("/content/waters").recurse {
-    activate(it.getPath())
+    if (getNode(it.getPath()).hasProperty("jcr:content/cq:lastReplicated")) {
+        if (OffsetDateTime.parse(getPage(it.getPath()).getContentResource().getValueMap().get("cq:lastReplicated", String.class)).format(DateTimeFormatter.ISO_LOCAL_DATE).compareTo(dateInstanceWentDown) >= 0)
+            activate(it.getPath())
+    }
 }
 
 getPage("/content/order").recurse {
-    activate(it.getPath())
+    if (getNode(it.getPath()).hasProperty("jcr:content/cq:lastReplicated")) {
+        if (OffsetDateTime.parse(getPage(it.getPath()).getContentResource().getValueMap().get("cq:lastReplicated", String.class)).format(DateTimeFormatter.ISO_LOCAL_DATE).compareTo(dateInstanceWentDown) >= 0)
+            activate(it.getPath())
+    }
 }
 
 getNode("/content/dam/waters").recurse {
-    activate(it.getPath())
+    if (getNode(it.getPath()).hasProperty("jcr:content/cq:lastReplicated")) {
+        if (OffsetDateTime.parse(getResource(it.getPath() + "/jcr:content").getValueMap().get("cq:lastReplicated", String.class)).format(DateTimeFormatter.ISO_LOCAL_DATE).compareTo(dateInstanceWentDown) >= 0)
+            activate(it.getPath())
+    }
 }
