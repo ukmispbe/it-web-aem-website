6afbe11f33ec137a384bf84545581ab32b73ab97
grvmishra556 <67579313+grvmishra556@users.noreply.github.com>
2020-10-26 19:07:50 +0530

    Pushing changes for default rollout location

--- waters-aem-core/src/main/java/com/waters/aem/core/constants/WatersConstants.java
+++ waters-aem-core/src/main/java/com/waters/aem/core/constants/WatersConstants.java
@@ -105,7 +105,7 @@ public final class WatersConstants {
 
     public static final String SEARCH_PAGE_PATH = ROOT_PATH_LANGUAGE_MASTERS + "/en/search.html";
 
-    public static final String DEFAULT_ROLLOUT_CONFIG_PATH = "/apps/msm/wcm/rolloutconfigs/default";
+    public static final String DEFAULT_ROLLOUT_CONFIG_PATH = "/libs/msm/wcm/rolloutconfigs/default";
     
     public static final int ORDER_LANGUAGE_ROOT = 2;
 
