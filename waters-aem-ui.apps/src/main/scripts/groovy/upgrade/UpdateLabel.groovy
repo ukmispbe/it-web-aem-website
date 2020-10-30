abc0f0d802db6a29aec19b9e8d594f4aaf2b2563
Sarthak Kakkar <sarkakka@publicisgroupe.net>
2020-10-30 16:28:10 +0530

    BTWCT-9308: Scripts for labels.

---
+++ waters-aem-ui.apps/src/main/scripts/groovy/upgrade/UpdateLabel.groovy
@@ -0,0 +1,24 @@
+def sites = [""] //"language-masters/en","us/en"
+def labels = ["":""] //"labelKey0":"labelValue0","labelKey1":"labelValue1"
+def data = []
+
+sites.each { site ->
+    def nodePath = "/content/waters/" + site + "/cart-checkout/jcr:content/root/labels/labelList"
+    if (getResource(nodePath) != null) {
+        getNode(nodePath).iterator().each {
+            labels.keySet().each { labelKey ->
+                if (it.hasProperty("labelKey") && it.getProperty("labelKey").getString().equals(labelKey)) {
+                    it.setProperty("labelValue", labels.get(labelKey))
+                    save()
+                    activate(it.getPath())
+                    data.add([nodePath, it.getPath()])
+                }
+            }
+        }
+    }
+}
+
+table {
+    columns("Parent", "Label")
+    rows(data)
+}
