9328a10f3e46166a053942970b0451561ac17385
Nakirikanti <santosh_nakirikanti@waters.com>
2021-01-12 09:14:15 -0500

    10187: added new filed for spellcheck with description

--- waters-aem-solr/src/main/java/com/waters/aem/solr/index/builder/AbstractSolrInputDocumentBuilder.java
+++ waters-aem-solr/src/main/java/com/waters/aem/solr/index/builder/AbstractSolrInputDocumentBuilder.java
@@ -201,9 +201,12 @@ public abstract class AbstractSolrInputDocumentBuilder implements SolrInputDocum
             addDate(document, page);
             //for eprocSpecific changes
             document.setField("eprocUrl", updateUrlForEprocBasedOnLocaleConfig(excludedLocales, page.getHref().replace(page.getParent(4).getPath(),WatersConstants.ORDER_ROOT_PATH)));
-            String[] eprocValues = new String [] {sku.getCode(),page.getTitle(TitleType.PAGE_TITLE).or(page.getTitle())};
+            String[] eprocSuggestValues = new String [] {sku.getCode(),page.getTitle(TitleType.PAGE_TITLE).or(page.getTitle())};
+            document.setField("autosuggest_skucontext",eprocSuggestValues );
 
-            document.setField("autosuggest_skucontext",eprocValues );
+            String[] eprocSpellcheckValues = new String [] {sku.getCode(),page.getTitle(TitleType.PAGE_TITLE).or(page.getTitle()),page.getDescription()};
+
+            document.setField("spellcheck_skucontext",eprocSpellcheckValues );
         }
     }
 
