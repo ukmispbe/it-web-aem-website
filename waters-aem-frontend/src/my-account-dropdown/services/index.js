8a2ef296a54149a73a7bd38f9a53919cd115b4e8
Mohd Danish <mohd.danish@publicissapient.com>
2021-02-03 13:27:25 +0530

    BTWCT-7511 localstorage remove cart id

--- waters-aem-frontend/src/my-account-dropdown/services/index.js
+++ waters-aem-frontend/src/my-account-dropdown/services/index.js
@@ -1,5 +1,6 @@
 import 'whatwg-fetch';
 import SessionStore from '../../stores/sessionStore';
+import LocalStore from '../../stores/localStore';
 
 const getData = async (url) => {
     const response = await fetch(url, {
@@ -16,6 +17,9 @@ export async function signOutRequest(url, signOutUrl, homepageLink) {
     const sessionStore = new SessionStore();
     sessionStore.removeUserDetails();
     sessionStore.removeSoldToDetails();
+    const localStore = new LocalStore();
+    localStore.removeGUID();
+    localStore.removeCartId();
 
     const response = await getData(url);
     //TODO: Remove after MyAccount-R-6.1.0 release
