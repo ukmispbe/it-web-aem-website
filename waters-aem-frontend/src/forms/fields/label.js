14c24641a86198306299ac8a2f7f3ce587f06a93
Sarah Dulaney <Sarah_Dulaney@waters.com>
2021-02-23 10:00:30 -0500

    BTWCT-11602: remove local testing data

--- waters-aem-frontend/src/forms/fields/label.js
+++ waters-aem-frontend/src/forms/fields/label.js
@@ -3,7 +3,7 @@ import { elementLocator } from '../../utils/eCommerceFunctions';
 
 const Label = ({ addClass, label, name, htmlFor ="" }) => {
     return (
-        <label className={addClass} htmlFor={htmlFor} data-locator={elementLocator('test123456')}>
+        <label className={addClass} htmlFor={htmlFor} data-locator={elementLocator(name)}>
             {label} 
         </label>   
     );
