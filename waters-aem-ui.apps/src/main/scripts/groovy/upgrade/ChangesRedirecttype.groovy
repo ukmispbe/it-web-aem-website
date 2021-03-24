1361295916ff20d91098c026356ff49e9ce0c054
conssha <sachin.sharma5@publicissapient.com>
2021-03-24 22:24:19 +0530

    convert 302 to 301 redirect

---
+++ waters-aem-ui.apps/src/main/scripts/groovy/upgrade/ChangesRedirecttype.groovy
@@ -0,0 +1,20 @@
+final def page = getPage('/content/waters/language-masters/en')
+final def template = '/conf/waters/settings/wcm/templates/content-page'
+final def query = buildQuery(page,template)
+final def result = query.execute()
+def buildQuery(page,template){
+    def queryManager = session.workspace.queryManager
+    def queryString ="SELECT * FROM [cq:Page] As node WHERE ISDESCENDANTNODE (node, [$page.path]) and node.[jcr:content/cq:template] = '$template' and node.[jcr:content/sling:redirectStatus] = '302'"
+    queryManager.createQuery(queryString, "JCR-SQL2")
+
+}
+result.nodes.each { node ->
+    println node
+    def jcrcontent= node.getNode("jcr:content")
+    if(jcrcontent.hasProperty('sling:redirectStatus')){
+        jcrcontent.setProperty('sling:redirectStatus', '301')
+        println "Updated " + jcrcontent.getPath() + "\n"
+    }
+
+}
+session.save()
