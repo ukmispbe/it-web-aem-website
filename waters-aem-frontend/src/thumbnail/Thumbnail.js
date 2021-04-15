c122d9f6ba211dbd2b14fbb6b704781c8dcdf3cf
Manish Dhar <manish.dwivedi@publicissapient.com>
2021-04-15 16:49:49 +0530

    thumbnail component with required basic features completed

---
+++ waters-aem-frontend/src/thumbnail/Thumbnail.js
@@ -0,0 +1,101 @@
+import React from "react";
+import ReactSVG from "react-svg";
+import PropTypes from "prop-types";
+import "./thumbnailStyle.css";
+
+const Thumbnail = ({
+  thumbnailURL,
+  thumbnailAltText,
+  thumbnailTitleText,
+  thumbnailLabel,
+  isVideo,
+  showVideoPlayButton,
+  timeStamp,
+  videoIconURL,
+  className,
+  thumbnailLabelClassName,
+  handleThumbnailMouseLeave,
+  handleThumbnailMouseOver,
+  handleThumbnailKeyUp,
+  onThumbnailClick,
+  handleThumbnailError,
+}) => (
+  <>
+    <button
+      type="button"
+      className={`thumbnail-container ${className}`}
+      onMouseLeave={handleThumbnailMouseLeave}
+      onMouseOver={handleThumbnailMouseOver}
+      onFocus={handleThumbnailMouseOver}
+      onKeyUp={handleThumbnailKeyUp}
+      onClick={onThumbnailClick}
+    >
+      <div className="thumbnail-img-inner">
+        {isVideo && showVideoPlayButton && (
+          <div className="thumbnail-video-overlay" />
+        )}
+        <img
+          src={thumbnailURL}
+          alt={thumbnailAltText}
+          title={thumbnailTitleText}
+          onError={handleThumbnailError}
+        />
+        {timeStamp && (
+          <div className="thumbnail-time-stamp-bg">
+            <span className="thumbnail-time-stamp-text">{timeStamp}</span>
+          </div>
+        )}
+        {isVideo && showVideoPlayButton && (
+          <div className="thumbnail-video-icon-bg">
+            <ReactSVG src={videoIconURL} />
+          </div>
+        )}
+      </div>
+    </button>
+    {thumbnailLabel && (
+      <div className={`thumbnail-title ${thumbnailLabelClassName}`}>
+        {thumbnailLabel}
+      </div>
+    )}
+  </>
+);
+
+Thumbnail.propTypes = {
+  thumbnailURL: PropTypes.string,
+  thumbnailAltText: PropTypes.string,
+  thumbnailTitleText: PropTypes.string,
+  thumbnailLabel: PropTypes.string,
+  isVideo: PropTypes.bool,
+  showVideoPlayButton: PropTypes.bool,
+  timeStamp: PropTypes.string,
+  videoIconURL: PropTypes.string,
+  className: PropTypes.string,
+  thumbnailLabelClassName: PropTypes.string,
+  handleThumbnailMouseLeave: PropTypes.func,
+  handleThumbnailMouseOver: PropTypes.func,
+  handleThumbnailMouseOver: PropTypes.func,
+  handleThumbnailKeyUp: PropTypes.func,
+  onThumbnailClick: PropTypes.func,
+  handleThumbnailError: PropTypes.func,
+};
+
+Thumbnail.defaultProps = {
+  thumbnailURL: "",
+  thumbnailAltText: "",
+  thumbnailTitleText: "",
+  thumbnailLabel: "",
+  isVideo: false,
+  showVideoPlayButton: true,
+  timeStamp: "",
+  videoIconURL: "",
+  className: "",
+  thumbnailLabelClassName: "",
+  handleThumbnailMouseLeave: () => {},
+  handleThumbnailMouseOver: () => {},
+  handleThumbnailMouseOver: () => {},
+  handleThumbnailKeyUp: () => {},
+  onThumbnailClick: () => {},
+  handleThumbnailError: () => {},
+};
+
+export default Thumbnail;
