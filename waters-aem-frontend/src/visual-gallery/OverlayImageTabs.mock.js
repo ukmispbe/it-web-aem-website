d4906ed4857463d1422354da5110fe41128171f5
Suraj Krishna <surkrish0@publicisgroupe.net>
2021-05-07 22:30:44 +0530

    Mock data object formatted

--- waters-aem-frontend/src/visual-gallery/OverlayImageTabs.mock.js
+++ waters-aem-frontend/src/visual-gallery/OverlayImageTabs.mock.js
@@ -1,8 +1,7 @@
 export const props = {
     templates: [{src: "http://localhost:4502/content/dam/waters/en/brand-assets/product/covid19/banner-covid-innovation.jpg.{{width}}.resize/img.jpg", alt: "test", description: "test", title: "test"},{src: "http://localhost:4502/content/dam/waters/en/brand-assets/product/catalogs/catalog-2019-2020.jpg.{{width}}.resize/img.jpg", alt: "alt text", description: "Some description", title: "test"}],
     widths: ['128','450','650','789','1280'],
-    zoomInIcon: "http://localhost:4502/content/dam/waters/en/brand-assets/icons/zoom-in.svg",
-      
+    zoomInIcon: "http://localhost:4502/content/dam/waters/en/brand-assets/icons/zoom-in.svg",      
     alt:"test",
     videoIds:['6084160447001', '5116024345001'],
     tabs:["Images","Videos"],
