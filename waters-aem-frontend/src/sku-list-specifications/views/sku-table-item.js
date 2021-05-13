491c193340a5546ae7ded5562549b5536a0a9a10
Ankit Upadhyay <ankit.upadhyay@publicissapient.com>
2021-05-13 16:40:06 +0530

    chore(): add comments for placeholder texts

--- waters-aem-frontend/src/sku-list-specifications/views/sku-table-item.js
+++ waters-aem-frontend/src/sku-list-specifications/views/sku-table-item.js
@@ -1,18 +1,25 @@
 import React from 'react';
 
 const SkuTableItem = () => {
-    return (
-        <li class="sku-table-item row">
-            <span className="item col-lg col-lg-1">1000000001</span>
-            <span className="item col-lg">
-                <a>lorem ipsum</a>
-            </span>
-            <span className="item col-lg col-lg-2">See Availability</span>
-            <span className="item col-lg col-lg-1 small">928 USD</span>
-            <span className="item col-lg col-lg-1">928 USD</span>
-            <span className="item col-lg col-lg-2"></span>
-        </li>
-    );
+  return (
+      /* Following are the placeholder values for the line item */
+      <li class="sku-table-item row">
+          {/* SKU Code */}
+          <span className="item col-lg col-lg-1">1000000001</span>
+          {/* SKU Title */}
+          <span className="item col-lg">
+              <a>lorem ipsum</a>
+          </span>
+          {/* SKU Availability */}
+          <span className="item col-lg col-lg-2">See Availability</span>
+          {/* SKU List Price */}
+          <span className="item col-lg col-lg-1 small">928 USD</span>
+          {/* SKU Customer Price */}
+          <span className="item col-lg col-lg-1">928 USD</span>
+          {/* Placeholder for add to cart */}
+          <span className="item col-lg col-lg-2"></span>
+      </li>
+  );
 };
 
 export default SkuTableItem;
