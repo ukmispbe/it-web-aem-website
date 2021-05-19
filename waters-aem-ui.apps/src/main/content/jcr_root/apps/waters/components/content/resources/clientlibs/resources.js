12826da946e748f4edd9ee04455fa81b253af50c
convkgk <convkgk@guest.waters.com>
2021-05-19 11:15:03 +0530

    Updated.

--- waters-aem-ui.apps/src/main/content/jcr_root/apps/waters/components/content/resources/clientlibs/resources.js
+++ waters-aem-ui.apps/src/main/content/jcr_root/apps/waters/components/content/resources/clientlibs/resources.js
@@ -19,12 +19,16 @@
 			var contentType = $dialog.find("foundation-autocomplete[name='./contentType']")
 			var tags = $dialog.find("foundation-autocomplete[name='./tags']")
 			var docNumber = $dialog.find("coral-multifield[data-granite-coral-multifield-name='./docNumber']");
+			var category= $dialog.find("coral-select[name='./category']");
+			var supportLibrary = $(category).children("coral-select-item[value='support%20library:Support%2520Library']");
+			var library = $(category).children("coral-select-item[value='library:Library']");
 
 			if (listType == "Tags") {			
 				contentType.parent().show();
 				tags.parent().show();
 				docNumber.parent().hide();
 				contentType.attr('required','');
+				library.attr('selected','');
 
 
 			} else if (listType == "docNumber") {
@@ -32,6 +36,7 @@
 				tags.parent().hide();
 				docNumber.parent().show();
 				contentType.removeAttr('required','');
+				supportLibrary.attr('selected','');
 			}
 
 		}
