293f0d12830cfc6168010e7d755389c9b56842c6
Suraj Krishna <surkrish0@publicisgroupe.net>
2021-05-20 21:25:04 +0530

    BTWCT-13335 | SKU Table snapshot testing

---
+++ waters-aem-frontend/src/sku-list-specifications/sku-table.mock.js
@@ -0,0 +1,30 @@
+export const props = {
+    config: {
+        skuInfo: {
+            listPriceLabel: 'Price Label',
+            custPriceLabel: 'Cust Price'
+        },
+        isCustomerPriceApiDisabled: true,
+        skuNumberLabel: 'SKU',
+        availabilityLabel: 'available',
+        productLabel: 'Product Label'
+    },
+    skuList: [],
+}
+
+export const skuListData = {
+    config: {
+        skuInfo: {
+            listPriceLabel: 'Price Label',
+            custPriceLabel: 'Cust Price'
+        },
+        isCustomerPriceApiDisabled: true,
+        skuNumberLabel: 'SKU',
+        availabilityLabel: 'available',
+        productLabel: 'Product Label'
+    },
+    skuList: [{
+        skucode: '12345678',
+        title: 'Test title'
+    }],
+}
